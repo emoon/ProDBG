db30f98452d038d8fb3efe9171edfb3192c00212
Daniel Collin <daniel@collin.com>
2014-07-24 11:16:43 +0200

    Removed win32 and added /DPRODBG_WIN

--- tundra.lua
+++ tundra.lua
@@ -25,27 +25,18 @@ local macosx = {
     Frameworks = { "Cocoa" },
 }
 
-local win32 = {
-    Env = {
-        GENERATE_PDB = "1",
-        CCOPTS = {
-            "/W4", "/I.", "/WX", "/DUNICODE", "/D_UNICODE", "/DWIN32", "/D_CRT_SECURE_NO_WARNINGS", "/wd4996", "/wd4389",
-            { "/Od"; Config = "*-*-debug" },
-            { "/O2"; Config = "*-*-release" },
-        },
-    },
-}
-
 local win64 = {
     Env = {
         GENERATE_PDB = "1",
         CCOPTS = {
+			"/DPRODBG_WIN",
             "/FS", "/MT", "/W4", "/I.", "/WX", "/DUNICODE", "/D_UNICODE", "/DWIN32", "/D_CRT_SECURE_NO_WARNINGS", "/wd4152", "/wd4996", "/wd4389",
             { "/Od"; Config = "*-*-debug" },
             { "/O2"; Config = "*-*-release" },
         },
 
         CXXOPTS = {
+			"/DPRODBG_WIN",
             "/FS", "/MT", "/I.", "/DUNICODE", "/D_UNICODE", "/DWIN32", "/D_CRT_SECURE_NO_WARNINGS", "/wd4152", "/wd4996", "/wd4389",
 			"\"/DOBJECT_DIR=$(OBJECTDIR:#)\"",
             { "/Od"; Config = "*-*-debug" },
