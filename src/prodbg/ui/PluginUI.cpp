cc18499c541ef59ee9cd100c139da38adf64f430
Daniel Collin <daniel@collin.com>
2014-07-29 16:16:20 +0200

    WIP on UISetup and PluginInstance createion

---
+++ src/prodbg/ui/PluginUI.cpp
@@ -0,0 +1,73 @@
+#include "PluginUI.h"
+#include <PDUI.h>
+#include <QListWidget>
+#include <QMdiSubWindow>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace prodbg
+{
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct PrivateData
+{
+	QWidget* window;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static PDUIListView listview_create(void* privateData, const char* name, int id)
+{
+	PrivateData* data = (PrivateData*)privateData;
+	QListWidget* listWidget = new QListWidget(data->window);
+	return (PDUIListView)listWidget; 
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static int listview_clear(void*, PDUIListView handle)
+{
+	QListWidget* listWidget = (QListWidget*)handle;
+	listWidget->clear();
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static int listview_item_add(void*, PDUIListView handle, const char* item)
+{
+	QListWidget* listWidget = (QListWidget*)handle;
+    QListWidgetItem* newItem = new QListWidgetItem;
+    newItem->setText(QString(QLatin1String(item)));
+    listWidget->insertItem(0, newItem);
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int listview_item_remove(void*, PDUIListView handle, int index)
+{
+	QListWidget* listWidget = (QListWidget*)handle;
+	listWidget->removeItemWidget(listWidget->itemAt(0, index));
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void PluginUI_init(QWidget* parent, PDUI* uiInstance)
+{
+	PrivateData* privData = new PrivateData;
+	privData->window = new QMdiSubWindow(parent);
+
+	uiInstance->listview_create = listview_create;
+	uiInstance->listview_item_add = listview_item_add;
+	uiInstance->listview_item_remove = listview_item_remove;
+	uiInstance->listview_item_text_get = 0;
+	uiInstance->privateData = privData;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
+
