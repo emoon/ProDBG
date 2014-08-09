bd41b9b66a32ee93ca55924ad0fe70bd16053ff1
Daniel Collin <daniel@collin.com>
2014-08-09 14:24:36 +0200

    Print the correct register data
    Also added correct event processing

--- src/plugins/registers/RegistersPlugin.cpp
+++ src/plugins/registers/RegistersPlugin.cpp
@@ -1,6 +1,7 @@
 #include <PDView.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <PDBackend.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -16,12 +17,11 @@ static void* createInstance(PDUI* uiFuncs, ServiceFunc* serviceFunc)
 	(void)serviceFunc;
 	RegistersData* userData = (RegistersData*)malloc(sizeof(RegistersData));
 	
-	static const char* foo[] = { "Register", "Value", 0 };
-	static const char* meh[] = { "d0", "0xfadebabe", 0 };
+	static const char* headers[] = { "Register", "Value", 0 };
 
-	userData->registerList = PDUIListView_create(uiFuncs, foo, 0);
+	userData->registerList = PDUIListView_create(uiFuncs, headers, 0);
 
-	PDUIListView_itemAdd(uiFuncs, userData->registerList, meh);
+	//PDUIListView_itemAdd(uiFuncs, userData->registerList, meh);
 
 	return userData;
 }
@@ -34,13 +34,75 @@ static void destroyInstance(void* userData)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// TODO: Support floats
+
+static void getRegisterString(char* value, PDReader* reader, PDReaderIterator it)
+{
+    uint64_t regValue;
+	uint32_t type = PDRead_findU64(reader, &regValue, "register", it);
+
+	switch (type & PDReadStatus_typeMask)
+	{
+		case PDReadType_u8: sprintf(value, "0x%02x", (uint8_t)regValue); break;
+		case PDReadType_s8: sprintf(value, "0x%02x", (int8_t)regValue); break;
+		case PDReadType_u16: sprintf(value, "0x%04x", (uint16_t)regValue); break;
+		case PDReadType_s16: sprintf(value, "0x%04x", (int16_t)regValue); break;
+		case PDReadType_u32: sprintf(value, "0x%08x", (uint32_t)regValue); break;
+		case PDReadType_s32: sprintf(value, "0x%08x", (int32_t)regValue); break;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void showInUI(RegistersData* data, PDReader* reader, PDUI* uiFuncs)
+{
+    PDReaderIterator it;
+
+    if (PDRead_findArray(reader, &it, "registers", 0) == PDReadStatus_notFound)
+    	return;
+
+	printf("registers found\n");
+
+	PDUIListView_clear(uiFuncs, data->registerList);
+
+    while (PDRead_getNextEntry(reader, &it))
+    {
+        uint64_t regValue;
+        const char* name = "";
+        char registerValue[128];
+
+        PDRead_findString(reader, &name, "name", it);
+		getRegisterString(registerValue, reader, it);
+
+		const char* values[] = { name, registerValue, 0 };
+
+		PDUIListView_itemAdd(uiFuncs, data->registerList, values);
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int update(void* userData, PDUI* uiFuncs, PDReader* inEvents, PDWriter* outEvents)
 {
-	(void)userData;
-	(void)inEvents;
+	uint32_t event;
+	RegistersData* data = (RegistersData*)userData;
+
+	// Loop over all the in events
+
+    while ((event = PDRead_getEvent(inEvents)) != 0)
+    {
+        switch (event)
+        {
+            case PDEventType_setRegisters : showInUI(data, inEvents, uiFuncs); break; 
+        }
+    }
+
+
+	//(void)userData;
+	//(void)inEvents;
 	(void)outEvents;
-	(void)uiFuncs;
+	//(void)uiFuncs;
 
 	return 0;
 }
