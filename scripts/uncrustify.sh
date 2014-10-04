5817e96081568d89ae6b67f0b285303666d87d36
Daniel Collin <daniel@collin.com>
2014-10-04 10:22:27 +0200

    Updated script

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -1,3 +1,3 @@
 rm uncrustify.lst 2> /dev/null
-find . -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -not -path "./testbed/examples/Fake6502/*" -not -path "./testbed/plugins/LLDB/*" > uncrustify.lst
-uncrustify -c uncrustify.cfg --no-backup -F uncrustify.lst
+find . -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -not -path "./examples/fake_6502/*" -not -path "./src/plugins/lldb/*" -not -path "./src/external/*" -not -path "./old/*" > uncrustify.lst
+bin/macosx/uncrustify -c scripts/uncrustify.cfg --no-backup -F uncrustify.lst
