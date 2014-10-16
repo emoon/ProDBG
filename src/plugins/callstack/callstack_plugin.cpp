f881eae74b1178728bc94bfd034136315e2bcf54
Daniel Collin <daniel@collin.com>
2014-10-16 15:02:50 +0200

    Show a proper callstack

--- src/plugins/callstack/callstack_plugin.cpp
+++ src/plugins/callstack/callstack_plugin.cpp
@@ -2,6 +2,7 @@
 #include <pd_backend.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -38,60 +39,81 @@ static void destroyInstance(void* userData)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TODO: Support floats
-/*
-   static void getAddressString(char* value, PDReader* reader, PDReaderIterator it)
-   {
-    uint64_t regValue;
-    uint32_t type = PDRead_findU64(reader, &regValue, "address", it);
+   
+static void getAddressString(char* value, PDReader* reader, PDReaderIterator it)
+{
+	uint64_t regValue;
+	uint32_t type = PDRead_findU64(reader, &regValue, "address", it);
 
-    switch (type & PDReadStatus_typeMask)
-    {
-        case PDReadType_u8: sprintf(value, "0x%02x", (uint8_t)regValue); break;
-        case PDReadType_s8: sprintf(value, "0x%02x", (int8_t)regValue); break;
-        case PDReadType_u16: sprintf(value, "0x%04x", (uint16_t)regValue); break;
-        case PDReadType_s16: sprintf(value, "0x%04x", (int16_t)regValue); break;
-        case PDReadType_u32: sprintf(value, "0x%08x", (uint32_t)regValue); break;
-        case PDReadType_s32: sprintf(value, "0x%08x", (int32_t)regValue); break;
-    }
-   }
- */
+	switch (type & PDReadStatus_typeMask)
+	{
+		case PDReadType_u8: sprintf(value, "0x%02x", (uint8_t)regValue); break;
+		case PDReadType_s8: sprintf(value, "0x%02x", (int8_t)regValue); break;
+		case PDReadType_u16: sprintf(value, "0x%04x", (uint16_t)regValue); break;
+		case PDReadType_s16: sprintf(value, "0x%04x", (int16_t)regValue); break;
+		case PDReadType_u32: sprintf(value, "0x%08x", (uint32_t)regValue); break;
+		case PDReadType_s32: sprintf(value, "0x%08x", (int32_t)regValue); break;
+		case PDReadType_u64: sprintf(value, "0x%014llx", (uint64_t)regValue); break;
+		case PDReadType_s64: sprintf(value, "0x%014llx", (int64_t)regValue); break;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int findSeparator(const char* str)
+{
+	size_t len = strlen(str);
+
+	for (size_t i = len; i != 0; --i)
+	{
+		if (str[i] == '/')
+			return (int)i + 1;
+	}
+
+	return 0;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void showInUI(CallStackData* data, PDReader* reader, PDUI* uiFuncs)
 {
     PDReaderIterator it;
+    (void)data;
 
     if (PDRead_findArray(reader, &it, "callstack", 0) == PDReadStatus_notFound)
         return;
 
-    (void)data;
-    (void)uiFuncs;
+	uiFuncs->text("");
 
-    /*
-       PDUIListView_clear(uiFuncs, data->callStackList);
+	// TODO: Have a "spec" for the callstack to be used
 
-       while (PDRead_getNextEntry(reader, &it))
-       {
-        const char* filename = "";
-        const char* module = "";
-        char address[64];
-        char lineText[64];
-        uint32_t line = 0;
+	uiFuncs->columns(4, "callstack", true);
+	uiFuncs->text("Address"); uiFuncs->nextColumn();
+	uiFuncs->text("Module"); uiFuncs->nextColumn();
+	uiFuncs->text("Name"); uiFuncs->nextColumn();
+	uiFuncs->text("Line"); uiFuncs->nextColumn();
 
-        getAddressString(address, reader, it);
+	while (PDRead_getNextEntry(reader, &it))
+	{
+		const char* filename = "";
+		const char* module = "";
+		char address[64] = { 0 };
+		uint32_t line = 0;
 
-        PDRead_findString(reader, &filename, "filename", it);
-        PDRead_findString(reader, &module, "module_name", it);
-        PDRead_findU32(reader, &line, "line", it);
+		getAddressString(address, reader, it);
 
-        sprintf(lineText, "%d", line);
+		PDRead_findString(reader, &filename, "filename", it);
+		PDRead_findString(reader, &module, "module_name", it);
+		PDRead_findU32(reader, &line, "line", it);
 
-        const char* values[] = { address, module, filename, lineText, 0 };
+		int fSep = findSeparator(filename);
+		int mSep = findSeparator(module);
 
-        PDUIListView_itemAdd(uiFuncs, data->callStackList, values);
-       }
-     */
+		uiFuncs->text(address); uiFuncs->nextColumn();
+		uiFuncs->text(&module[mSep]); uiFuncs->nextColumn();
+		uiFuncs->text(&filename[fSep]); uiFuncs->nextColumn();
+		uiFuncs->text("%d", line); uiFuncs->nextColumn();
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -112,7 +134,8 @@ static int update(void* userData, PDUI* uiFuncs, PDReader* inEvents, PDWriter* o
 
     // Request callstack data
 
-    PDWrite_eventBegin(outEvents, PDEventType_getDisassembly);
+    PDWrite_eventBegin(outEvents, PDEventType_getCallstack);
+    PDWrite_u8(outEvents, "dummy_remove", 0);	// TODO: Remove me
     PDWrite_eventEnd(outEvents);
 
     return 0;
