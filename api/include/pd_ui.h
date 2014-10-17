ece70fdea691e5907395919cb2896f077a957daa
Daniel Collin <daniel@collin.com>
2014-10-17 09:00:00 +0200

    Expose all layout functions for Imgui

--- api/include/pd_ui.h
+++ api/include/pd_ui.h
@@ -12,13 +12,32 @@ struct PDRect;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+typedef struct PDVec2
+{
+	float x, y;
+} PDVec2;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 typedef struct PDUI
 {
     // Layout
-
+	void (*separator)();                                         // horizontal line
+	void (*sameLine)(int columnX, int spacingW);                 // call between widgets to layout them horizontally
+	void (*spacing)();
     void (*columns)(int count, const char* id, int border);
     void (*nextColumn)();
-    void (*sameLine)();
+	float (*getColumnOffset)(int column_index);
+	void (*setColumnOffset)(int column_index, float offset);
+	float (*getColumnWidth)(int column_index);
+	PDVec2 (*getCursorPos)();                                    // cursor position relative to window position
+	void (*setCursorPos)(PDVec2 pos);                            // "
+	void (*setCursorPosX)(float x);                              // "
+	void (*setCursorPosY)(float y);                              // "
+	PDVec2 (*getCursorScreenPos)();                              // cursor position in screen space
+	void (*alignFirstTextHeightToWidgets)();                     // call once if the first item on the line is a Text() item and you want to vertically lower it to match subsequent (bigger) widgets.
+	float (*getTextLineSpacing)();
+	float (*getTextLineHeight)();
 
     // Text
 
