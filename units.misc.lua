d5df8a263449554bb63327b6057cff759d271b17
unknown <graham@FB-WORKFLOW2.dice.ad.ea.com>
2014-11-16 12:50:34 +0100

    Added as_game to units.misc.lua

--- units.misc.lua
+++ units.misc.lua
@@ -51,7 +51,41 @@ Program {
 }
 
 -----------------------------------------------------------------------------------------------------------------------
+-- Example AngelScript game
+
+Program {
+    Name = "as_game",
+
+    Env = {
+        CCOPTS = {
+            { 
+            "-Wno-conversion", 
+            "-Wno-missing-variable-declarations",
+            "-Werror", 
+            "-Wno-pedantic", 
+            "-Wno-conversion",
+            "-Wno-missing-field-initializers",
+            "-Wno-conversion",
+            "-Wno-switch-enum",
+            "-Wno-format-nonliteral"; Config = "macosx-*-*" },
+        },
+    },
+
+    Sources = { 
+        Glob {
+            Dir = "examples/as_game",
+            Extensions = { ".h", ".cpp" },
+        },
+    },
+
+    Libs = { { "wsock32.lib", "kernel32.lib" ; Config = { "win32-*-*", "win64-*-*" } } },
+
+    Depends = { "remote_api" },
+}
+
+-----------------------------------------------------------------------------------------------------------------------
 
 Default "fake6502"
 Default "crashing_native"
+Default "as_game"
 
