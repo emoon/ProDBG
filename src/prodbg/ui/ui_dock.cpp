4c59e24cf386c24406b4e3d747528e587dcb025c
Daniel Collin <daniel@collin.com>
2014-12-23 10:09:41 +0100

    WIP on Docking

--- src/prodbg/ui/ui_dock.cpp
+++ src/prodbg/ui/ui_dock.cpp
@@ -5,16 +5,6 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-struct ViewPluginInstance;
-
-struct UIDockView
-{
-	ViewPluginInstance* viewInstance;
-	Rect rect; 
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 enum UIDockSizerDir
 {
 	UIDockSizerDir_Horz,
@@ -23,10 +13,18 @@ enum UIDockSizerDir
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+enum UIDockType
+{
+	UIDockType_Docked,
+	UIDockType_Floating,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct UIDockSizer
 {
-	UIDockView* side0;	// up/left
-	UIDockView* side1;	// down/right
+	ViewPluginInstance* side0;	// up/left
+	ViewPluginInstance* side1;	// down/right
 	UIDockSizerDir dir;
 	Rect rect; 
 };
@@ -35,11 +33,13 @@ struct UIDockSizer
 
 struct UIDock
 {
-	struct UIDockSizer* topSizer;
-	struct UIDockSizer* bottomSizer;
-	struct UIDockSizer* rightSizer;
-	struct UIDockSizer* leftSizer;
-	struct UIDockView currentView;
+	UIDockSizer* topSizer;
+	UIDockSizer* bottomSizer;
+	UIDockSizer* rightSizer;
+	UIDockSizer* leftSizer;
+	ViewPluginInstance* view;
+
+	UIDockType type;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,51 +76,100 @@ UIDockingGrid* UIDock_createGrid(Rect* rect)
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// This will check if we haven't attached any view to the dock we just set it
-
-static bool checkFirstAttach(UIDock* dock, ViewPluginInstance* instance)
-{
-	if (dock->currentView.viewInstance)
-		return false;
-
-	dock->currentView.viewInstance = instance;
-	dock->currentView.rect = instance->rect;
-
-	return true;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 UIDock* UIDock_addView(UIDockingGrid* grid, ViewPluginInstance* view)
 {
-	(void)grid;
-	(void)view;
+	UIDock* dock = (UIDock*)alloc_zero(sizeof(UIDock)); 
 
+	dock->view = view;
 
+	// If this is the first we we just set it as maximized
 
-	return 0;
+	if (grid->docks.size() == 0)
+		dock->view->rect = grid->rect;
+
+	// TODO: How should we really handle this case?
+
+	grid->docks.push_back(dock);
+
+	return dock;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void UIDock_dockLeft(UIDock* dock, ViewPluginInstance* instance)
+static Rect calcVerticalSizerSize(const Rect* rect)
 {
-	if (checkFirstAttach(dock, instance))
-		return;
+	Rect sizerRect;
 
-	Rect rect = dock->currentView.rect;
+	sizerRect.x = rect->x; 
+	sizerRect.y = rect->y; 
+	sizerRect.width = g_sizerSize;
+	sizerRect.height = rect->height;
 
-	// TODO: Support non-even split 
-	//
+	return sizerRect;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void UIDock_dockLeft(UIDockingGrid* grid, UIDock* dock, ViewPluginInstance* instance)
+{
+	UIDock* newDock = (UIDock*)alloc_zero(sizeof(UIDock));
+
+	newDock->view = instance;
+	Rect rect = dock->view->rect;
 
 	// Push current window forward 
 
-	dock->currentView.rect.y += rect.width;
-	dock->currentView.rect.width = rect.width;
+	dock->view->rect.y += rect.width;
+	dock->view->rect.width = rect.width;
+
+	// Setup the rect for the new dock
 
 	rect.width = int_min(rect.width - g_sizerSize, 0);
+	newDock->view->rect = rect;
+	
+	// Check if the window that we dock to has any sizer of the left size
+	// and if it doesn't (already on the left edge of the grid) we need
+	// to create a new one
 
+	UIDockSizer* sizer = (UIDockSizer*)alloc_zero(sizeof(UIDockSizer));
+	Rect sizerRect = calcVerticalSizerSize(&rect);
 
+	sizer->side0 = newDock->view; 
+	sizer->side1 = dock->view; 
+	sizer->dir = UIDockSizerDir_Vert;
+	sizer->rect = sizerRect;
+
+	if (!dock->leftSizer)
+	{
+		// Attach the sizer
+
+		dock->leftSizer = sizer;
+		newDock->rightSizer = sizer;
+	}
+	else
+	{
+		// If there already is a sizer we are getting connected between two windows
+		//
+		// |------|------|
+		// | Left | Old  |
+		// | View | View |
+		// |------|------|
+		//
+		// -->
+		// -->
+		//        os      ns
+		// |------|-------|------|
+		// | Left | New   | Old  |
+		// | View | View  | View |
+		// |------|-------|------|
+		//
+		// Is what the result should be and the old view is what gets split
+
+		dock->leftSizer->side1 = instance;
+	}
+
+	grid->sizers.push_back(sizer);
 	                                
 }
 /*
