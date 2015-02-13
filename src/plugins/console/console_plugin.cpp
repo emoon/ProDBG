8320d0a712ba85cbcf0074a234c5d9fc68fc41d8
Daniel Collin <daniel@collin.com>
2015-02-13 19:36:28 +0100

    Removed some dummy tests

--- src/plugins/console/console_plugin.cpp
+++ src/plugins/console/console_plugin.cpp
@@ -361,16 +361,6 @@ static int update(void* userData, PDUI* uiFuncs, PDReader* inEvents, PDWriter* o
     uint32_t event = 0;
     (void)event;
 
-	// testing
-	//oe oesuth
-	// dummy
-    // dummy
-    // sotehusoeth
-    // soethusoeth
-    // sotehusoe
-    // stoehuso
-    // eusths
-
     /*while ((event = PDRead_getEvent(inEvents)) != 0)
        {
         switch (event)
