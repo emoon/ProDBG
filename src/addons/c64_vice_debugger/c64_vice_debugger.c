7858600681e9e239e454b13f867aafb3ab7fc703
Daniel Collin <daniel@collin.com>
2015-03-02 14:11:10 +0100

    WIP on Registes
    Ref T13

--- src/addons/c64_vice_debugger/c64_vice_debugger.c
+++ src/addons/c64_vice_debugger/c64_vice_debugger.c
@@ -48,28 +48,10 @@ typedef struct PluginData
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static void* createInstance(ServiceFunc* serviceFunc)
-{
-	(void)serviceFunc;
-	
-	PluginData* data = malloc(sizeof(PluginData));
-
-	data->state = PDDebugState_noTarget;
-
-	return data;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void destroyInstance(void* userData)
-{
-	free(userData);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 static void connectToLocalHost(PluginData* data)
 {
+	struct VICEConnection* conn = 0; 
+
 	// Kill the current connection if we have one
 
 	if (data->conn)
@@ -78,7 +60,7 @@ static void connectToLocalHost(PluginData* data)
 		data->conn = 0;
 	}
 
-	struct VICEConnection* conn = VICEConnection_create(VICEConnectionType_Connect, 6510);
+	conn = VICEConnection_create(VICEConnectionType_Connect, 6510);
 
 	if (!VICEConnection_connect(conn, "localhost", 6510))
 	{
@@ -90,10 +72,36 @@ static void connectToLocalHost(PluginData* data)
 		return;
 	}
 
-	// VICE state when connecting seems to be break so we set it as such
-
 	data->conn = conn;
-	data->state = PDDebugState_stopBreakpoint;
+	data->state = PDDebugState_running;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void* createInstance(ServiceFunc* serviceFunc)
+{
+	(void)serviceFunc;
+	
+	PluginData* data = malloc(sizeof(PluginData));
+	memset(data, 0, sizeof(PluginData));
+
+	data->state = PDDebugState_noTarget;
+
+	connectToLocalHost(data);
+
+	return data;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void destroyInstance(void* userData)
+{
+    PluginData* plugin = (PluginData*)userData;
+
+    if (plugin->conn)
+		VICEConnection_destroy(plugin->conn);
+
+	free(plugin);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -119,6 +127,7 @@ static void onMenu(PluginData* data, PDReader* reader)
 static void sendCommand(PluginData* data, const char* command)
 {
 	int len = (int)strlen(command);
+	printf("sending command: %s\n", command);
 	VICEConnection_send(data->conn, command, len, 0);
 }
 
@@ -158,23 +167,98 @@ static int getData(PluginData* data, char** resBuffer, int* len)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+struct Regs6510
+{
+	uint16_t pc; 
+	uint16_t a; 
+	uint16_t x; 
+	uint16_t y; 
+	uint16_t sp; 
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void writeRegister(PDWriter* writer, const char* name, uint8_t size, uint16_t reg, uint8_t readOnly)
+{
+    PDWrite_arrayEntryBegin(writer);
+    PDWrite_string(writer, "name", name);
+    PDWrite_u8(writer, "size", size);
+
+    if (readOnly)
+        PDWrite_u8(writer, "read_only", 1);
+
+    if (size == 2)
+    	PDWrite_u16(writer, "register", reg);
+	else
+    	PDWrite_u8(writer, "register", (uint8_t)reg);
+
+    PDWrite_arrayEntryEnd(writer);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void parseRegisters(struct Regs6510* regs, char* str)
+{
+	// Format from VICE looks like this:
+	// (C:$e5cf)   ADDR AC XR YR SP 00 01 NV-BDIZC LIN CYC  STOPWATCH
+	//           .;e5cf 00 00 0a f3 2f 37 00100010 000 001    3400489
+	//
+	
+  	const char* pch = strtok(str," \t\n"); 
+
+  	// Skip the layout (hard-coded and ugly but as registers are fixed this should be fine
+  	// but needs to be fixed if VICE changes the layout
+
+  	for (int i = 0; i < 12; ++i)
+		pch = strtok(0, " \t\n");
+
+	regs->pc = (uint16_t)strtol(&pch[2], 0, 16); pch = strtok(0, " \t");
+	regs->a = (uint8_t)strtol(pch, 0, 16); pch = strtok(0, " \t");
+	regs->x = (uint8_t)strtol(pch, 0, 16); pch = strtok(0, " \t");
+	regs->y = (uint8_t)strtol(pch, 0, 16); pch = strtok(0, " \t");
+	regs->sp = (uint8_t)strtol(pch, 0, 16); pch = strtok(0, " \t");
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 static void setRegisters(PluginData* data, PDWriter* writer)
 {
+	struct Regs6510 regs;
 	char* res = 0; 
 	int len = 0;
 
 	(void)writer;
 
-	sendCommand(data, "registers");
+	sendCommand(data, "registers\n");
+
+	printf("waiting to get data back\n");
 
 	// Try to get some data back
 
 	if (!getData(data, &res, &len))
+	{
+		printf("No data back!\n");
 		return;
+	}
 
-	// TODO: Parse here
+	// Always stop on command
 
-	printf("setRegisters: data back %s\n", res);
+	data->state = PDDebugState_stopException;
+
+	parseRegisters(&regs, res);
+
+    PDWrite_eventBegin(writer, PDEventType_setRegisters);
+    PDWrite_arrayBegin(writer, "registers");
+
+    writeRegister(writer, "pc", 2, regs.pc, 1);
+    writeRegister(writer, "sp", 1, regs.sp, 0);
+    writeRegister(writer, "a", 1, regs.a, 0);
+    writeRegister(writer, "x", 1, regs.x, 0);
+    writeRegister(writer, "y", 1, regs.y, 0);
+    //writeRegister(writer, "status", 1, status, 1);
+
+    PDWrite_arrayEnd(writer);
+    PDWrite_eventEnd(writer);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -191,7 +275,7 @@ static void processEvents(PluginData* data, PDReader* reader, PDWriter* writer)
         {
             //case PDEventType_getExceptionLocation : setExceptionLocation(plugin, writer); break;
             //case PDEventType_getCallstack : setCallstack(plugin, writer); break;
-            case PDEventType_setRegisters : setRegisters(data, writer); break;
+            case PDEventType_getRegisters : setRegisters(data, writer); break;
 			case PDEventType_menuEvent : onMenu(data, reader);
         }
     }
@@ -205,6 +289,8 @@ static void processEvents(PluginData* data, PDReader* reader, PDWriter* writer)
 static PDDebugState update(void* userData, PDAction action, PDReader* reader, PDWriter* writer)
 {
 	(void)action;
+
+	printf("c64_vice_update\n");
 
     PluginData* plugin = (PluginData*)userData;
 
