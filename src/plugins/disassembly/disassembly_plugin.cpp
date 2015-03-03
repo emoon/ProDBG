6e1c75a62a002af7a76e5357014d8ceb1d8b4a58
Daniel Collin <daniel@collin.com>
2015-03-04 00:05:25 +0100

    Use new disassembly format that is array based
    This makes it more robust of checking the current PC which would just do a strstr before and now we can do a proper check with a number value instead that is written down by the backend which has the context of the address which the disasembly plugin may or may not have

--- src/plugins/disassembly/disassembly_plugin.cpp
+++ src/plugins/disassembly/disassembly_plugin.cpp
@@ -3,69 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct Line
 {
+    uint64_t address;
     const char* text;
-    bool breakpoint;        // TODO: Flags
+    bool breakpoint;
+    uint8_t addressSize;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct DisassemblyRange
+{
+    Line* lines;
+    int lineCount;
+    uint64_t rangeStart;
+    uint64_t rangeEnd;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct DissassemblyData
 {
-    char* code;
-    Line* lines;
-    int lineCount;
-    uint64_t pc;
-    uint64_t location;
-    uint8_t locationSize;
+	DisassemblyRange range;
+	uint64_t location;
+	uint64_t pc;
+	uint8_t locationSize;
 };
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-static void splitIntoLines(DissassemblyData* data, char* code, size_t size)
-{
-    int lineCount = 0;
-    Line* lines;
-
-    char* target = code;
-    char* targetEnd = code + size;
-
-    free(data->lines);
-
-    // so this is really waste of memory but will do for now
-
-    data->lines = lines = (Line*)malloc(sizeof(Line) * size);
-    memset(lines, 0, sizeof(Line) * size);
-
-    lines[0].text = target;
-    lineCount++;
-
-    while (target < targetEnd)
-    {
-        char c = *target;
-
-        if (*target == '\r')
-        {
-            *target++ = 0;
-            c = *target;
-        }
-
-        if (*target == '\n')
-        {
-            *target = 0;
-            lines[lineCount++].text = target + 1;
-        }
-
-        target++;
-    }
-
-    *target++ = 0;
-    data->lineCount = lineCount;
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -90,51 +58,76 @@ static void destroyInstance(void* userData)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static void setDisassemblyCode(DissassemblyData* data, PDReader* inEvents)
+static void freeRange(DisassemblyRange* range)
 {
-    const char* stringBuffer;
+	for (int i = 0, end = range->lineCount; i < end; ++i)
+		free((void*)range->lines[i].text);
 
-    PDRead_findString(inEvents, &stringBuffer, "string_buffer", 0);
+	free(range->lines);
 
-    //printf("Got disassembly\n");
-    //printf("disassembly %s\n", stringBuffer);
+	range->lineCount = 0;
+}
 
-    free(data->code);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    if (!stringBuffer)
+static void setDisassemblyCode(DissassemblyData* data, PDReader* reader)
+{
+	Line* line;
+
+    PDReaderIterator it;
+
+    if (PDRead_findArray(reader, &it, "disassembly", 0) == PDReadStatus_notFound)
         return;
 
-    data->code = strdup(stringBuffer);
+	freeRange(&data->range);
 
-    splitIntoLines(data, data->code, strlen(stringBuffer));
+    int linesCount = 0;
+
+    PDReaderIterator t = it;
+
+    while (PDRead_getNextEntry(reader, &t))
+    {
+    	linesCount++;
+    }
+
+    data->range.lines = line = (Line*)malloc((size_t)linesCount * sizeof(Line));
+    data->range.lineCount = linesCount;
+
+    while (PDRead_getNextEntry(reader, &it))
+    {
+    	const char* text;
+    	line->breakpoint = false;
+
+		line->addressSize = (uint8_t)PDRead_findU64(reader, &line->address, "address", it);
+
+		PDRead_findString(reader, &text, "line", it);
+
+		line->text = strdup(text);
+
+		line++;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void renderUI(DissassemblyData* data, PDUI* uiFuncs)
 {
-    if (!data->code)
-        return;
-
     uiFuncs->text("");  // TODO: Temporary
 
-    char tempBuffer[64];
-    sprintf(tempBuffer, "%04X", (uint16_t)data->pc);
-
-    for (int i = 0; i < data->lineCount; ++i)
+    for (int i = 0; i < data->range.lineCount; ++i)
     {
-        if (strstr(data->lines[i].text, tempBuffer))
+        if (data->range.lines[i].address == data->pc) 
         {
             PDRect rect;
             PDVec2 pos = uiFuncs->getCursorPos();
             rect.x = pos.x;
-            rect.y = pos.y - 11;
-            rect.width = -1;
+            rect.y = pos.y;
+            rect.width = 400;
             rect.height = 14;
             uiFuncs->fillRect(rect, PD_COLOR_32(200, 0, 0, 127));
         }
 
-        uiFuncs->text(data->lines[i].text);
+        uiFuncs->text("0x%04x %s", (uint64_t)data->range.lines[i].address, data->range.lines[i].text);
     }
 }
 
@@ -195,9 +188,14 @@ static int update(void* userData, PDUI* uiFuncs, PDReader* inEvents, PDWriter* w
 
     // Temporary req
 
+	int pc = (int)(data->pc) - 0x20;
+
+	if (pc < 0)
+		pc = 0;
+
     PDWrite_eventBegin(writer, PDEventType_getDisassembly);
-    PDWrite_u64(writer, "address_start", 0);
-    PDWrite_u32(writer, "instruction_count", (uint32_t)10);
+    PDWrite_u64(writer, "address_start", (uint64_t)pc); 
+    PDWrite_u32(writer, "instruction_count", (uint32_t)20);
     PDWrite_eventEnd(writer);
 
     return 0;
@@ -220,10 +218,10 @@ extern "C"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    PD_EXPORT void InitPlugin(RegisterPlugin* registerPlugin, void* privateData)
-    {
-        registerPlugin(PD_VIEW_API_VERSION, &plugin, privateData);
-    }
+PD_EXPORT void InitPlugin(RegisterPlugin* registerPlugin, void* privateData)
+{
+	registerPlugin(PD_VIEW_API_VERSION, &plugin, privateData);
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
