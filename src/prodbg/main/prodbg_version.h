17808133dc54b39b3c5f02eda5a4867b5751c43a
Daniel Collin <daniel@collin.com>
2015-04-25 11:21:58 +0200

    More build testing

--- src/prodbg/main/prodbg_version.h
+++ src/prodbg/main/prodbg_version.h
@@ -3,9 +3,9 @@
 // This file may or may not be auto generated (yes, really)
 
 #ifdef PRODBG_WIN
-#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 11:20:12 2015 (Git: 273f174079)"
+#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 11:21:43 2015 (Git: 1555a24e77)"
 #elif PRODBG_MAC
-#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 11:20:12 2015 (Git: 273f174079)"
+#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 11:21:43 2015 (Git: 1555a24e77)"
 #else
-#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 11:20:12 2015 (Git: 273f174079)"
+#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 11:21:43 2015 (Git: 1555a24e77)"
 #endif
