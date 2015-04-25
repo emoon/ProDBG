e81a101050da0f4d87d50f507933968fb707bb83
Daniel Collin <daniel@collin.com>
2015-04-25 11:17:23 +0200

    Bump header to trigger rebuild

--- src/prodbg/main/prodbg_version.h
+++ src/prodbg/main/prodbg_version.h
@@ -3,9 +3,9 @@
 // This file may or may not be auto generated (yes, really)
 
 #ifdef PRODBG_WIN
-#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 10:22:25 2015 (Git: 8325a2a89d)"
+#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 11:17:09 2015 (Git: 95e1a4d03b)"
 #elif PRODBG_MAC
-#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 10:22:25 2015 (Git: 8325a2a89d)"
+#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 11:17:09 2015 (Git: 95e1a4d03b)"
 #else
-#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 10:22:25 2015 (Git: 8325a2a89d)"
+#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 11:17:09 2015 (Git: 95e1a4d03b)"
 #endif
