8325a2a89dca5751f4806391dff833a9239c0081
Daniel Collin <daniel@collin.com>
2015-04-25 10:21:46 +0200

    Bump version (buildbot testing)

--- src/prodbg/main/prodbg_version.h
+++ src/prodbg/main/prodbg_version.h
@@ -3,9 +3,9 @@
 // This file may or may not be auto generated (yes, really)
 
 #ifdef PRODBG_WIN
-#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 10:18:10 2015 (Git: a704f066a6)"
+#define PRODBG_VERSION L"ProDBG 0.01 Sat Apr 25 10:21:29 2015 (Git: e0b23b56bf)"
 #elif PRODBG_MAC
-#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 10:18:10 2015 (Git: a704f066a6)"
+#define PRODBG_VERSION @"ProDBG 0.01 Sat Apr 25 10:21:29 2015 (Git: e0b23b56bf)"
 #else
-#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 10:18:10 2015 (Git: a704f066a6)"
+#define PRODBG_VERSION "ProDBG 0.01 Sat Apr 25 10:21:29 2015 (Git: e0b23b56bf)"
 #endif
