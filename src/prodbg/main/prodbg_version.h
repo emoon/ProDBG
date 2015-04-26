9741ecd51d1f6b76e343e20a41469abce2a994f9
Daniel Collin <daniel@collin.com>
2015-04-26 16:45:36 +0200

    Bump version for Monkey

--- src/prodbg/main/prodbg_version.h
+++ src/prodbg/main/prodbg_version.h
@@ -3,9 +3,9 @@
 // This file may or may not be auto generated (yes, really)
 
 #ifdef PRODBG_WIN
-#define PRODBG_VERSION L"ProDBG 0.01 Sun Apr 26 16:39:21 2015 (Git: f16b9dc0c0)"
+#define PRODBG_VERSION L"ProDBG 0.01 Sun Apr 26 16:45:22 2015 (Git: 9d08d6bab0)"
 #elif PRODBG_MAC
-#define PRODBG_VERSION @"ProDBG 0.01 Sun Apr 26 16:39:21 2015 (Git: f16b9dc0c0)"
+#define PRODBG_VERSION @"ProDBG 0.01 Sun Apr 26 16:45:22 2015 (Git: 9d08d6bab0)"
 #else
-#define PRODBG_VERSION "ProDBG 0.01 Sun Apr 26 16:39:21 2015 (Git: f16b9dc0c0)"
+#define PRODBG_VERSION "ProDBG 0.01 Sun Apr 26 16:45:22 2015 (Git: 9d08d6bab0)"
 #endif
