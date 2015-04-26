9d7be4a381a4c2c04bc78ee48bad547f17cf243c
Daniel Collin <daniel@collin.com>
2015-04-26 16:52:23 +0200

    Debugging stuff for coverage monkey

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -1,2 +1,4 @@
 #/bin/bash
+echo "hum..."
 coveralls --exclude-pattern ".*as_debugger.*" --exclude-pattern ".*as_game.*" --exclude-pattern ".*Frameworks.*" --exclude-pattern ".*external.*" --exclude-pattern ".*glsl.*" --exclude-pattern ".*examples.*" -x cpp -x c
+echo "Hum end!"
