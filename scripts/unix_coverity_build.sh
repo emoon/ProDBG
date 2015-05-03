5575bd73f825c4002ba84c68820ee110e09a262f
Daniel Collin <daniel@collin.com>
2015-05-03 17:01:49 +0200

    Enable curl upload

--- scripts/unix_coverity_build.sh
+++ scripts/unix_coverity_build.sh
@@ -5,4 +5,4 @@ rm -rfv prodbg_cov.xz
 tundra2 -c linux-gcc-release
 cov-build --dir cov-int tundra2 linux-gcc-release
 tar cavf prodbg_cov.xz cov-int
-# curl --form token=gBMZ9SVfbnqwMUGd7JkIvA --form email=daniel@collin.com --form file=@prodbg_cov.xz --form version="0.01" --form description="Linux GCC" https://scan.coverity.com/builds?project=emoon%2FProDBG
+curl --form token=gBMZ9SVfbnqwMUGd7JkIvA --form email=daniel@collin.com --form file=@prodbg_cov.xz --form version="0.01" --form description="Linux GCC" https://scan.coverity.com/builds?project=emoon%2FProDBG
