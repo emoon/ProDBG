24df5b94008f6c01be24de1df3fb408b3e6e6b38
Daniel Collin <daniel@collin.com>
2015-05-03 09:29:30 +0200

    Better error handling in loadToMemory

--- src/prodbg/core/file.cpp
+++ src/prodbg/core/file.cpp
@@ -7,8 +7,8 @@
 void* File_loadToMemory(const char* filename, size_t* size, size_t padAllocSize)
 {
     FILE* f = fopen(filename, "rb");
-    void* data;
-    size_t s;
+    void* data = 0;
+    size_t s = 0, t = 0;
 
     *size = 0;
 
@@ -18,17 +18,26 @@ void* File_loadToMemory(const char* filename, size_t* size, size_t padAllocSize)
     // TODO: Use fstat here?
 
     fseek(f, 0, SEEK_END);
-    s = (size_t)ftell(f);
-    fseek(f, 0, SEEK_SET);
+    long ts = ftell(f);
+
+    if (ts < 0)
+    	goto end;
+
+    s = (size_t)ts;
 
     data = malloc(s + padAllocSize);
 
     if (!data)
-        return 0;
+    	goto end;
 
-    fread(data, s, 1, f);
+    fseek(f, 0, SEEK_SET);
+
+    t = fread(data, s, 1, f);
+    (void)t;
 
     *size = s;
+
+end:
 
     fclose(f);
 
