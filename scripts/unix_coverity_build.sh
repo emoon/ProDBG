0d4729faef15d86154d9660e1bfe9509c0089741
Daniel Collin <daniel@collin.com>
2015-05-06 19:16:15 +0200

    Verbose output when building

--- scripts/unix_coverity_build.sh
+++ scripts/unix_coverity_build.sh
@@ -3,6 +3,6 @@
 rm -rfv cov-int
 rm -rfv prodbg_cov.xz
 tundra2 -c linux-gcc-release
-cov-build --dir cov-int tundra2 linux-gcc-release
+cov-build --dir cov-int tundra2 -v linux-gcc-release
 tar cavf prodbg_cov.xz cov-int
 curl --form token=gBMZ9SVfbnqwMUGd7JkIvA --form email=daniel@collin.com --form file=@prodbg_cov.xz --form version="0.01" --form description="Linux GCC" https://scan.coverity.com/builds?project=emoon%2FProDBG
