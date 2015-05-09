eadbeaa30373f0b27a6610623044d2df9d4c367d
Daniel Collin <daniel@collin.com>
2015-05-09 09:26:22 +0200

    Misc cleanup

--- src/prodbg/core/core.cpp
+++ src/prodbg/core/core.cpp
@@ -8,10 +8,6 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//extern "C" void foundation_hack_environment_main_args(int argc, const char* const* argv);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 void Core_init()
 {
 	static application_t application;
@@ -22,10 +18,6 @@ void Core_init()
 	application.version = foundation_version();
 	application.flags = APPLICATION_UTILITY;
 	application.dump_callback = 0;
-
-	// static const char* const temp[] = { "temp" };
-
-	//foundation_hack_environment_main_args(1, temp);
 
 	foundation_initialize(memory_system_malloc(), application);
 }
