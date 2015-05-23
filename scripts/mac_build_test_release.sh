41c67aa683a4eca6a1807a7c3ad8951d56629168
Daniel Collin <daniel@collin.com>
2015-05-23 17:43:45 +0200

    Hack to work-around mac-coverage crash

--- scripts/mac_build_test_release.sh
+++ scripts/mac_build_test_release.sh
@@ -2,3 +2,5 @@
 
 rm -rf t2-output/macosx_test-clang-release-default
 bin/macosx/tundra/tundra2 macosx_test-clang-release
+bin/macosx/tundra/tundra2 macosx_test-clang-release
+bin/macosx/tundra/tundra2 macosx_test-clang-release
