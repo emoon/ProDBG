ce73568f2d7976cde55c7e1343cbcd147eb7d71e
Marco Castorina <marco@marcocastorina.com>
2015-06-04 22:48:33 +0100

    Fixed build issue with clang

--- src/external/imgui/imgui.cpp
+++ src/external/imgui/imgui.cpp
@@ -699,7 +699,8 @@ const float PI = 3.14159265358979323846f;
 #define IM_INT_MIN  INT_MIN
 #define IM_INT_MAX  INT_MAX
 #else
-#define IM_INT_MIN  (-2147483647-1)
+// NOTE(marco): fixed clang compile issue (macro not used)
+//#define IM_INT_MIN  (-2147483647-1)
 #define IM_INT_MAX  (2147483647)
 #endif
 
