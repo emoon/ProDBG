b6642544b284d3a55e933abfbc7523d525d6e661
Daniel Collin <daniel@collin.com>
2015-06-27 21:56:41 +0200

    Created a dummy thread on posix/clang

--- examples/crashing_native/crash.c
+++ examples/crashing_native/crash.c
@@ -2,11 +2,50 @@
 
 extern void callMe();
 
+#ifdef __clang__
+
+#include <pthread.h>
+#include <unistd.h>
+#include <stdint.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void* dummyThreadFunc(void* dummy)
+{
+	pthread_setname_np("dummy_thread");
+	(void)dummy;
+
+	if (((uintptr_t)dummy) == 1)
+		return 0;
+ 
+ 	while (1)
+	{
+		usleep(1000);
+	}
+
+	//return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void createDummyThread()
+{
+	pthread_t thread = 0;
+
+	pthread_create(&thread, 0, dummyThreadFunc, 0);
+}
+
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     int i;
+
+#ifdef __clang__
+	createDummyThread();
+#endif
 
     for (i = 0; i < 20; ++i)
         printf("%d\n", i);
