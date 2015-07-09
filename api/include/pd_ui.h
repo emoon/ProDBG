1b6e77cf92c8d4afc60d1ce7c9b189acc8f0464a
Daniel Collin <daniel@collin.com>
2015-07-09 08:29:05 +0200

    Working on exposing most of ImGui

--- api/include/pd_ui.h
+++ api/include/pd_ui.h
@@ -15,6 +15,7 @@ extern "C" {
 
 struct PDUIPainter;
 struct PDRect;
+typedef struct PDTextureID PDTextureID;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -37,6 +38,8 @@ typedef struct PDRect
 	float x, y;
 	float width, height;
 } PDRect;
+
+typedef unsigned int PDColor;
 
 // TODO: use uint32_t?
 
@@ -134,89 +137,235 @@ typedef struct PDSCInterface
 
 typedef struct PDUI
 {
-    // Layout
-	void (*separator)();                                         // horizontal line
-	void (*sameLine)(int columnX, int spacingW);                 // call between widgets to layout them horizontally
-	void (*spacing)();
-    void (*columns)(int count, const char* id, int border);
-    void (*nextColumn)();
-	float (*getColumnOffset)(int column_index);
-	void (*setColumnOffset)(int column_index, float offset);
-	float (*getColumnWidth)(int column_index);
-	PDVec2 (*getCursorPos)();                                    // cursor position relative to window position
-	void (*setCursorPos)(PDVec2 pos);                            // "
-	void (*setCursorPosX)(float x);                              // "
-	void (*setCursorPosY)(float y);                              // "
-	PDVec2 (*getCursorScreenPos)();                              // cursor position in screen space
-	void (*alignFirstTextHeightToWidgets)();                     // call once if the first item on the line is a Text() item and you want to vertically lower it to match subsequent (bigger) widgets.
-	float (*getTextLineSpacing)();
-	float (*getTextLineHeight)();
-	float (*getTextWidth)(const char* text, const char* textEnd);
-    void (*setScrollHere)();
-    void (*pushItemWidth)(float item_width);                     // width of items for the common item+label case. default to ~2/3 of windows width.
-    void (*popItemWidth)();
-
 	// Window
 	
 	void (*setTitle)(void* privateData, const char* title);
 	PDVec2 (*getWindowSize)();
 	PDVec2 (*getWindowPos)();
-	float (*getFontHeight)();
-	float (*getFontWidth)();
 
     void (*beginChild)(const char* stringId, PDVec2 size, bool border, int extraFlags /* PDWindowFlags */);
     void (*endChild)();
 
-    // Popup
-    
-    // mark popup as open. popup identifiers are relative to the current ID-stack (so OpenPopup and BeginPopup needs to be at the same level). 
-    // close childs popups if any. will close popup when user click outside, or activate a pressable item, or 
-    // CloseCurrentPopup() is called within a BeginPopup()/EndPopup() block.
-    void (*openPopup)(const char* strId);
+	float (*getScrollPosY)();
+	float (*getScrollMaxY)();
+	void (*setScrollPosHere)();
+	void (*setKeyboardFocusHere)(int offset);
 
-    // return true if popup if opened and start outputting to it. only call EndPopup() if BeginPopup() returned true!
-    int (*beginPopup)(const char* strId);                                    
+	// Parameters stacks (shared)
+	void (*pushFont)(ImFont* font);
+	void (*popFont)();
+	void (*pushStyleColor)(ImGuiCol idx, CONST struct ImVec4 col);
+	void (*popStyleColor)(int count);
+	void (*pushStyleVar)(ImGuiStyleVar idx, float val);
+	void (*pushStyleVarVec)(ImGuiStyleVar idx, CONST struct ImVec2 val);
+	void (*popStyleVar)(int count);
 
-    // modal dialog (can't close them by clicking outside)
-    int (*beginPopupModal)(const char* name, int* opened, int flags);             
+	// Parameters stacks (current window)
+	void (*pushItemWidth)(float item_width);
+	void (*popItemWidth)();
+	float (*calcItemWidth)();
+	void (*pushAllowKeyboardFocus)(bool v);
+	void (*popAllowKeyboardFocus)();
+	void (*pushTextWrapPos)(float wrap_pos_x);
+	void (*popTextWrapPos)();
+	void (*pushButtonRepeat)(bool repeat);
+	void (*popButtonRepeat)();
 
-    // helper to open and begin popup when clicked on last item
-    int (*beginPopupContextItem)(const char* strID, int mouseButton);                                        
 
-    // helper to open and begin popup when clicked on current window
-    int (*beginPopupContextWindow)(int alsoOverItems, const char* strId, int mouseButton);  
+	// Tooltip
+	void (*setTooltip)(const char* fmt, ...);
+	void (*setTooltipV)(const char* fmt, va_list args);
+	void (*beginTooltip)();
+	void (*endTooltip)();
 
-    // helper to open and begin popup when clicked in void (no window)
-    int (*beginPopupContextVoid)(const char* strId, int mouseButton);                                 
+	// Popup
+	void (*openPopup)(const char* strId);
+	bool (*beginPopup)(const char* strId);
+	bool (*beginPopupModal)(const char* name, bool* p_opened, ImGuiWindowFlags extraFlags);
+	bool (*beginPopupContextItem)(const char* strId, int mouse_button);
+	bool (*beginPopupContextWindow)(bool also_over_items, const char* strId, int mouse_button);
+	bool (*beginPopupContextVoid)(const char* strId, int mouse_button);
+	void (*endPopup)();
+	void (*closeCurrentPopup)();
 
-	// End the popup
-    void (*endPopup)();
+	// Layout
+	void (*beginGroup)();
+	void (*endGroup)();
+	void (*separator)();
+	void (*sameLine)(int column_x, int spacing_w);
+	void (*spacing)();
+	void (*dummy)(const ImVec2* size);
+	void (*indent)();
+	void (*unindent)();
+	void (*columns)(int count, const char* id, bool border);
+	void (*nextColumn)();
+	int  (*getColumnIndex)();
+	float(*getColumnOffset)(int column_index);
+	void (*setColumnOffset)(int column_index, float offset_x);
+	float (*getColumnWidth)(int column_index);
+	int (*getColumnsCount)();
+	void (*getCursorPos)(PDVec2* pOut);
+	float (*getCursorPosX)();
+	float (*getCursorPosY)();
+	void (*setCursorPos)(const PDVec2 pos);
+	void (*setCursorPosX)(float x);
+	void (*setCursorPosY)(float y);
+	void (*getCursorScreenPos)(PDVec2* pOut);
+	void (*setCursorScreenPos)(const PDVec2 pos);
+	void (*alignFirstTextHeightToWidgets)();
+	float(*getTextLineHeight)();
+	float (*getTextLineHeightWithSpacing)();
+	float (*getItemsLineHeightWithSpacing)();
 
-    // close the popup we have begin-ed into. clicking on a MenuItem or Selectable automatically close the current popup.
-    void (*closeCurrentPopup)();                                                
+	// ID scopes
+	// If you are creating widgets in a loop you most likely want to push a unique identifier so ImGui can differentiate them
+	// You can also use "##extra" within your widget name to distinguish them from each others (see 'Programmer Guide')
+	void (*pushIdStr)(const char* strId);
+	void (*pushIdStrRange)(const char* str_begin, const char* str_end);
+	void (*pushIdPtr)(const void* ptr_id);
+	void (*pushIdInt)(const int int_id);
+	void (*popId)();
+	ImGuiID (*getIdStr)(const char* strId);
+	ImGuiID (*getIdStrRange)(const char* str_begin,const char* str_end);
+	ImGuiID (*getIdPtr)(const void* ptr_id);
+
+	// Widgets
+	void (*text)(const char* fmt, ...);
+	void (*textV)(const char* fmt, va_list args);
+	void (*textColored)(const PDColor col, const char* fmt, ...);
+	void (*textColoredV)(const PDColor col, const char* fmt, va_list args);
+	void (*textDisabled)(const char* fmt, ...);
+	void (*textDisabledV)(const char* fmt, va_list args);
+	void (*textWrapped)(const char* fmt, ...);
+	void (*textWrappedV)(const char* fmt, va_list args);
+	void (*textUnformatted)(const char* text, const char* text_end);
+	void (*labelText)(const char* label, const char* fmt, ...);
+	void (*labelTextV)(const char* label, const char* fmt, va_list args);
+	void (*bullet)();
+	void (*bulletText)(const char* fmt, ...);
+	void (*bulletTextV)(const char* fmt, va_list args);
+	bool (*button)(const char* label, const PDVec2 size);
+	bool (*smallButton)(const char* label);
+	bool (*invisibleButton)(const char* strId, const PDVec2 size);
+	void (*image)(PDTextureID user_texture_id, const PDVec2 size, const PDVec2 uv0, const PDVec2 uv1, const PDColor tintColor, const PDColor borderColor);
+	bool (*imageButton)(PDTextureID user_texture_id, const PDVec2 size, const PDVec2 uv0, const PDVec2 uv1, int framePadding, const PDColor bgColor, const PDColor tintCol);
+	bool (*collapsingHeader)(const char* label, const char* strId, bool displayFrame, bool defaultOpen);
+	bool (*checkbox)(const char* label, bool* v);
+	bool (*checkboxFlags)(const char* label, unsigned int* flags, unsigned int flagsValue);
+	bool (*radioButtonBool)(const char* label, bool active);
+	bool (*radioButton)(const char* label, int* v, int v_button);
+	bool (*combo)(const char* label, int* currentItem, const char** items, int itemsCount, int heightInItems);
+	bool (*combo2)(const char* label, int* currentItem, const char* itemsSeparatedByZeros, int heightInItems);
+	bool (*combo3)(const char* label, int* currentItem, bool(*itemsGetter)(void* data, int idx, const char** out_text), void* data, int itemsCount, int heightInItems);
+	bool (*colorButton)(const PDVec4 col, bool smallHeight, bool outlineBorder);
+	bool (*colorEdit3)(const char* label, float col[3]);
+	bool (*colorEdit4)(const char* label, float col[4], bool showAlpha);
+	void (*colorEditMode)(ImGuiColorEditMode mode);
+	void (*plotLines)(const char* label, const float* values, int valuesCount, int valuesOffset, const char* overlayText, float scaleMin, float scaleMax, PDVec2 graphSize, size_t stride);
+	void (*plotLines2)(const char* label, float(*values_getter)(void* data, int idx), void* data, int valuesCount, int valuesOffset, const char* overlayText, float scaleMin, float scaleMax, PDVec2 graphSize);
+	void (*plotHistogram)(const char* label, const float* values, int valuesCount, int valuesOffset, const char* overlayText, float scaleMin, float scaleMax, PDVec2 graphSize, size_t stride);
+	void (*plotHistogram2)(const char* label, float(*valuesGetter)(void* data, int idx), void* data, int valuesCount, int valuesOffset, const char* overlayText, float scaleMin, float scaleMax, PDVec2 graphSize);
+
+	// Widgets: Scintilla text interface
+	PDSCInterface* (*scInputText)(const char* label, float xSize, float ySize, void (*callback)(void), void* userData);
+
+	// Widgets: Sliders (tip: ctrl+click on a slider to input text)
+	bool (*sliderFloat)(const char* label, float* v, float vMin, float vMax, const char* displayFormat, float power);
+	bool (*sliderFloat2)(const char* label, float v[2], float vMin, float vMax, const char* displayFormat, float power);
+	bool (*sliderFloat3)(const char* label, float v[3], float vMin, float vMax, const char* displayFormat, float power);
+	bool (*sliderFloat4)(const char* label, float v[4], float vMin, float vMax, const char* displayFormat, float power);
+	bool (*sliderAngle)(const char* label, float* v_rad, float vDegreesMin, float vDegreesMax);
+	bool (*sliderInt)(const char* label, int* v, int vMin, int vMax, const char* displayFormat);
+	bool (*sliderInt2)(const char* label, int v[2], int vMin, int vMax, const char* displayFormat);
+	bool (*sliderInt3)(const char* label, int v[3], int vMin, int vMax, const char* displayFormat);
+	bool (*sliderInt4)(const char* label, int v[4], int vMin, int vMax, const char* displayFormat);
+	bool (*vsliderFloat)(const char* label, const PDVec2 size, float* v, float vMin, float vMax, const char* displayFormat, float power);
+	bool (*vsliderInt)(const char* label, const PDVec2 size, int* v, int vMin, int vMax, const char* displayFormat);
+
+	// Widgets: Drags (tip: ctrl+click on a drag box to input text)
+	bool (*dragFloat)(const char* label, float* v, float vSpeed, float vMin, float vMax, const char* displayFormat, float power);     // If vMax >= vMax we have no bound
+	bool (*dragFloat2)(const char* label, float v[2], float vSpeed, float vMin, float vMax, const char* displayFormat, float power);
+	bool (*dragFloat3)(const char* label, float v[3], float vSpeed, float vMin, float vMax, const char* displayFormat, float power);
+	bool (*dragFloat4)(const char* label, float v[4], float vSpeed, float vMin, float vMax, const char* displayFormat, float power);
+	bool (*dragInt)(const char* label, int* v, float vSpeed, int vMin, int vMax, const char* displayFormat);                                       // If vMax >= vMax we have no bound
+	bool (*dragInt2)(const char* label, int v[2], float vSpeed, int vMin, int vMax, const char* displayFormat);
+	bool (*dragInt3)(const char* label, int v[3], float vSpeed, int vMin, int vMax, const char* displayFormat);
+	bool (*dragInt4)(const char* label, int v[4], float vSpeed, int vMin, int vMax, const char* displayFormat);
+
+	// Widgets: Input
+    bool (*inputText)(const char* label, char* buf, int buf_size, int flags, void (*callback)(PDInputTextCallbackData), void* userData);
+	bool (*inputTextMultiline)(const char* label, char* buf, size_t buf_size, const ImVec2 size, ImGuiInputTextFlags flags, ImGuiTextEditCallback callback, void* userData);
+	bool (*inputFloat)(const char* label, float* v, float step, float step_fast, int decimal_precision, ImGuiInputTextFlags extraFlags);
+	bool (*inputFloat2)(const char* label, float v[2], int decimal_precision, ImGuiInputTextFlags extraFlags);
+	bool (*inputFloat3)(const char* label, float v[3], int decimal_precision, ImGuiInputTextFlags extraFlags);
+	bool (*inputFloat4)(const char* label, float v[4], int decimal_precision, ImGuiInputTextFlags extraFlags);
+	bool (*inputInt)(const char* label, int* v, int step, int step_fast, ImGuiInputTextFlags extraFlags);
+	bool (*inputInt2)(const char* label, int v[2], ImGuiInputTextFlags extraFlags);
+	bool (*inputInt3)(const char* label, int v[3], ImGuiInputTextFlags extraFlags);
+	bool (*inputInt4)(const char* label, int v[4], ImGuiInputTextFlags extraFlags);
+
+	// Widgets: Trees
+	bool (*treeNode)(const char* str_label_id);
+	bool (*treeNodeStr)(const char* strId, const char* fmt, ...);
+	bool (*treeNodePtr)(const void* ptr_id, const char* fmt, ...);
+	bool (*treeNodeStrV)(const char* strId, const char* fmt, va_list args);
+	bool (*treeNodePtrV)(const void* ptr_id, const char* fmt, va_list args);
+	void (*treePushStr)(const char* strId);
+	void (*treePushPtr)(const void* ptr_id);
+	void (*treePop)();
+	void (*setNextTreeNodeOpened)(bool opened, ImGuiSetCond cond);
+
+	// Widgets: Selectable / Lists
+	bool (*selectable)(const char* label, bool selected, ImGuiSelectableFlags flags, const ImVec2 size);
+	bool (*selectableEx)(const char* label, bool* p_selected, ImGuiSelectableFlags flags, const ImVec2 size);
+	bool (*listBox)(const char* label, int* currentItem, const char** items, int itemsCount, int heightInItems);
+	bool (*listBox2)(const char* label, int* currentItem, bool(*items_getter)(void* data, int idx, const char** out_text), void* data, int itemsCount, int heightInItems);
+	bool (*listBoxHeader)(const char* label, const PDVec2 size);
+	bool (*listBoxHeader2)(const char* label, int itemsCount, int heightInItems);
+	void (*listBoxFooter)();
+
+	// Widgets: Menus
+	bool (*beginMainMenuBar)();
+	void (*endMainMenuBar)();
+	bool (*beginMenuBar)();
+	void (*endMenuBar)();
+	bool (*beginMenu)(const char* label, bool enabled);
+	void (*endMenu)();
+	bool (*menuItem)(const char* label, const char* shortcut, bool selected, bool enabled);
+	bool (*menuItemPtr)(const char* label, const char* shortcut, bool* p_selected, bool enabled);
+
+	// Widgets: Value() Helpers. Output single value in "name: value" format (tip: freely declare your own within the ImGui namespace!)
+	void (*valueBool)(const char* prefix, bool b);
+	void (*valueInt)(const char* prefix, int v);
+	void (*valueUInt)(const char* prefix, unsigned int v);
+	void (*valueFloat)(const char* prefix, float v, const char* float_format);
+	void (*color)(const char* prefix, const PDVec4 v);
+	void (*color2)(const char* prefix, unsigned int v);
+
+	// Logging: all text output from interface is redirected to tty/file/clipboard. Tree nodes are automatically opened.
+	void (*logToTTY)(int max_depth);
+	void (*logToFile)(int max_depth, const char* filename);
+	void (*logToClipboard)(int max_depth);
+	void (*logFinish)();
+	void (*logButtons)();
+	void (*logText)(const char* fmt, ...);
+
 
 
     // Text
 
-    void (*text)(const char* fmt, ...);
-    void (*textColored)(PDVec4 col, const char* fmt, ...);
-    void (*textWrapped)(const char* fmt, ...);
-    bool (*inputText)(const char* label, char* buf, int buf_size, int flags, void (*callback)(PDInputTextCallbackData*), void* userData);
 
     // Scintilla Editor Widget
 	
-	PDSCInterface* (*scInputText)(const char* label, float xSize, float ySize, void (*callback)(void*), void* userData);
+	PDSCInterface* (*scInputText)(const char* label, float xSize, float ySize, void (*callback)(void), void* userData);
 
     // Widgets
 
     int (*checkbox)(const char* label, bool* v);
-    int (*button)(const char* label);
     int (*buttonSmall)(const char* label);
-    int (*buttonSize)(const char* label, int width, int height);
 
     int (*selectableFixed)(const char* label, int selected, int flags, PDVec2 size); 
     int (*selectable)(const char* label, int* selected, int flags, PDVec2 size);
-
 
     // Misc
 
