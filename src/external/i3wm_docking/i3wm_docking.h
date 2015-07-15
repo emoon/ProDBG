73abc7997c9ea02bb80cac67a770b3f437d16382
Daniel Collin <daniel@collin.com>
2015-07-15 10:35:28 +0200

    Added wrapper header for i3wm_docking
    Part of T191

---
+++ src/external/i3wm_docking/i3wm_docking.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include "include/data.h"
+#include "include/con.h"
+#include "include/tree.h"
+#include "include/move.h"
+#include "include/log.h"
+#include "include/output.h"
+#include "include/workspace.h"
+#include "include/callbacks.h"
+
+#ifdef __cplusplus
+}
+#endif
+
