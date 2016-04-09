758ba96b0f29b8a83c8a068eed750d34539867a3
Daniel Collin <daniel@collin.com>
2016-04-09 15:13:46 +0200

    Cleanup test stuff

--- src/prodbg/main/src/main.rs
+++ src/prodbg/main/src/main.rs
@@ -13,7 +13,7 @@ mod backend_plugin;
 use docking::DockingPlugin;
 //use prodbg_api::ui::Ui;
 use bgfx_rs::Bgfx;
-use imgui_sys::Imgui;
+//use imgui_sys::Imgui;
 
 use session::Sessions;
 use windows::Windows;
@@ -24,13 +24,7 @@ use std::rc::Rc;
 
 use core::plugins::*;
 
-//const WIDTH: usize = 1280;
-//const HEIGHT: usize = 1024;
-
-//const MENU_CREATE_VIEW_0: usize = 2;
-//const MENU_CREATE_VIEW_1: usize = 3;
-//const MENU_CREATE_VIEW_2: usize = 4;
-
+/*
 fn add_view(index: usize, sessions: &mut Sessions, windows: &mut Windows, view_plugins: &mut ViewPlugins) {
     let session = sessions.get_current();
     let window = windows.get_current();
@@ -41,20 +35,6 @@ fn add_view(index: usize, sessions: &mut Sessions, windows: &mut Windows, view_p
         window.add_view(handle);
         session.add_view(handle);
     });
-}
-
-/*
-   fn menu_press(id: usize, sessions: &mut Sessions, windows: &mut Windows, view_plugins: &ViewPlugins) {
-   match id {
-   MENU_CREATE_VIEW_0 => {
-//add_view(0, sessions, windows, view_plugins);
-}
-_ => (),
-}
-}
-
-fn create_menus() -> Vec<MenuInfo> {
-
 }
 */
 
@@ -81,20 +61,17 @@ fn main() {
 
     plugins.add_plugin(&mut lib_handler, "bitmap_memory");
     plugins.add_plugin(&mut lib_handler, "i3_docking");
-    //plugins.add_plugin(&mut lib_handler, "dummy_backend");
-
-    // TODO: Wrap away this code.
 
     windows.create_default();
 
-    add_view(0, &mut sessions, &mut windows, &mut view_plugins.borrow_mut());
+    //add_view(0, &mut sessions, &mut windows, &mut view_plugins.borrow_mut());
 
     loop {
         bgfx.pre_update();
 
         plugins.update(&mut lib_handler);
         sessions.update(&mut view_plugins.borrow_mut());
-        windows.update();
+        windows.update(&mut view_plugins.borrow_mut());
 
         if windows.should_exit() {
             break;
