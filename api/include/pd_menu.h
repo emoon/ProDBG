e9b896f5a5fd3c7a282fc16f65d3d3d5b7bf7ff2
Daniel Collin <daniel@collin.com>
2016-05-06 13:14:06 +0200

    Making the menu code into a service instead
    Less marshaling of data between langs.

--- api/include/pd_menu.h
+++ api/include/pd_menu.h
@@ -1,28 +1,40 @@
-#ifndef _PRODBGAPI_MENU_H_
-#define _PRODBGAPI_MENU_H_
+#ifndef _PRODBGAPI_MENU_SERVICE_H_
+#define _PRODBGAPI_MENU_SERVICE_H_ 
 
 #include "pd_keys.h"
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define PDMENUFUNCS_GLOBAL "Menu Service 1"
 
-typedef struct PDMenuItem {
-    const char* name;
-    unsigned int id;
-    int key;
-    int mac_mod;
-    int win_mod;
-} PDMenuItem;
+typedef uint64_t PDMenuHandle;
+typedef uint64_t PDMenuItem;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-typedef struct PDMenu {
-    const char* name;
-    PDMenuItem* items;
-} PDMenu;
+enum {
+	PDMENU_FLAGS_ENABLED = 1 << 1,
+	PDMENU_FLAGS_DISABLED = 1 << 2,
+	PDMENU_FLAGS_SEPARATOR = 1 << 3,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct PDMenuFuncs {
+	PDMenuHandle (*create_menu)(const char* name);
+	void (*insert_menu)(PDMenuHandle parent, PDMenuHandle child);
+	void (*destroy_menu)(PDMenuHandle handle);
+
+	PDMenuItem (*insert_menu_item)(PDMenuHandle menu, const char* name, uint32_t id);
+	void (*remove_menu_item)(PDMenuItem handle);
+
+	void (*set_flags)(PDMenuItem item, uint32_t flags);
+	void (*set_shortcut_key)(PDMenuItem item, uint32_t accel_key, uint32_t modifier);
+
+} PDMenuFuncs;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
