9eeb51ea2fe7f29f7726695be2fc1e3004c40a90
Daniel Collin <daniel@collin.com>
2016-05-15 13:44:31 +0200

    DIsassembly support

--- src/addons/amiga_uae_plugin/src/lib.rs
+++ src/addons/amiga_uae_plugin/src/lib.rs
@@ -4,23 +4,22 @@ extern crate libc;
 use prodbg_api::*;
 use libc::c_void;
 pub mod gdb;
-use gdb::{GdbRemote, NeedsAck};
+use gdb::{GdbRemote, Memory, NeedsAck};
 //use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr};
 //use std::io::Read;
 //use std::{slice, str};
 
 //use std::ptr;
 
-//static CODE: &'static [u8] = b"\x55\x48\x8b\x05\xb8\x13\x00\x00";
+//static CODE: &'static [u8] = b"\xd4\x40\x87\x5a\x4e\x71\x02\xb4\xc0\xde\xc0\xde\x5c\x00\x1d\x80\x71\x12\x01\x23\xf2\x3c\x44\x22\x40\x49\x0e\x56\x54\xc5\xf2\x3c\x44\x00\x44\x7a\x00\x00\xf2\x00\x0a\x28\x4E\xB9\x00\x00\x00\x12\x4E\x75";
 
 struct AmigaUaeBackend {
-    _capstone: Capstone,
+    capstone: Capstone,
     conn: gdb::GdbRemote,
     exception_location: u32,
 }
 
 impl AmigaUaeBackend {
-
     fn from_hex(ch: u8) -> u8 {
         if (ch >= b'a') && (ch <= b'f') {
             return ch - b'a' + 10;
@@ -70,9 +69,7 @@ impl AmigaUaeBackend {
         for i in 0..8 {
             let name = format!("a{}", i);
             let reg = Self::get_u32(&data[index..]);
-
             Self::write_register(writer, &name, reg, false);
-
             index += 8;
         }
 
@@ -81,9 +78,7 @@ impl AmigaUaeBackend {
         for i in 0..8 {
             let name = format!("d{}", i);
             let reg = Self::get_u32(&data[index..]);
-
             Self::write_register(writer, &name, reg, false);
-
             index += 8;
         }
 
@@ -101,9 +96,47 @@ impl AmigaUaeBackend {
         writer.event_end();
     }
 
+    fn write_disassembly(&mut self, writer: &mut Writer, mem: &Memory) {
+        match self.capstone.open(Arch::M68K, CS_MODE_M68K_000) {
+            Err(e) => {
+                println!("Unable to open Capstone {}", e as i32);
+                return;
+            }
+            _ => (),
+        }
+
+        if let Ok(insns) = self.capstone.disasm(&mem.data, mem.address, 0) {
+            writer.event_begin(EventType::SetDisassembly as u16);
+            writer.array_begin("disassembly");
+
+            for i in insns.iter() {
+                let text = format!("{}\t{}", i.mnemonic().unwrap(), i.op_str().unwrap_or(""));
+                writer.array_entry_begin();
+                writer.write_u32("address", i.address as u32);
+                writer.write_string("line", &text);
+
+                //println!("{:?}", i);
+
+                writer.array_entry_end();
+            }
+
+            writer.array_end();
+            writer.event_end();
+        } else {
+            println!("No instructions :(");
+        }
+    }
+
+    fn write_exception_location(&mut self, writer: &mut Writer) {
+        writer.event_begin(EventType::SetExceptionLocation as u16);
+        writer.write_u64("address", self.exception_location as u64);
+        writer.write_u8("size", 4);
+        writer.event_end();
+    }
+
     /*
     fn test_capstone(&mut self) {
-        match self.capstone.open(Arch::X86, MODE_64) {
+        match self.capstone.open(Arch::M68K, CS_MODE_M68K_000) {
             Err(e) => {
                 println!("Unable to open Capstone {}", e as i32);
                 return;
@@ -127,7 +160,7 @@ impl AmigaUaeBackend {
 impl Backend for AmigaUaeBackend {
     fn new(service: &Service) -> Self {
         AmigaUaeBackend {
-            _capstone: service.get_capstone(),
+            capstone: service.get_capstone(),
             conn: GdbRemote::new(NeedsAck::No),
             exception_location: 0,
         }
@@ -157,6 +190,12 @@ impl Backend for AmigaUaeBackend {
                 if let Some(data) = self.conn.get_registers_sync() {
                     self.write_registers(writer, &data.clone());
                 }
+
+                if let Some(memory) = self.conn.get_memory_sync(self.exception_location - 128, 256) {
+                    self.write_disassembly(writer, &memory);
+                }
+
+                self.write_exception_location(writer);
             }
             _ => (),
         }
@@ -167,9 +206,7 @@ impl Backend for AmigaUaeBackend {
 
     fn register_menu(&mut self, menu_funcs: &mut MenuFuncs) -> *mut c_void {
         let menu = menu_funcs.create_menu("Amiga UAE Debugger");
-
         menu_funcs.add_menu_item(menu, "Connect to UAE...", 0, 0, 0);
-
         menu
     }
 }
