f3e44fd6a10e0644e6b9c4c6930e26d37a6f5588
Daniel Collin <daniel@collin.com>
2016-05-27 18:54:14 +0200

    Memory viewer now fetches from relative address

--- src/plugins/hex_memory/hex_memory_plugin.cpp
+++ src/plugins/hex_memory/hex_memory_plugin.cpp
@@ -12,10 +12,10 @@ struct HexMemoryData {
     int dataSize;
     int addressSize;
     char startAddress[64];
-    char endAddress[64];
+    char sizeText[64];
     bool requestData;
     uint64_t sa;
-    uint64_t ea;
+    uint64_t size;
     uint64_t exceptionLocation;
 };
 
@@ -29,21 +29,21 @@ static void* createInstance(PDUI* uiFuncs, ServiceFunc* serviceFunc) {
     memset(user_data, 0, sizeof(HexMemoryData));
 
     strcpy(user_data->startAddress, "0x00000000");
-    strcpy(user_data->endAddress, "0x00001000");
+    strcpy(user_data->sizeText, "1024");
 
     user_data->sa = 0;
-    user_data->ea = 0x00000fff;
+    user_data->size = 1024;
 
-    user_data->data = (unsigned char*)malloc(1024 * 1024);
-    user_data->addressSize = 2;
+    user_data->data = (unsigned char*)malloc(128 * 1024);
+    user_data->addressSize = 4;
 
-    user_data->oldData = (unsigned char*)malloc(1024 * 1024);
-    user_data->addressSize = 2;
+    user_data->oldData = (unsigned char*)malloc(128 * 1024);
+    user_data->addressSize = 4;
 
     // clear
 
-    memset(user_data->data, 0xff, 1024 * 1024);
-    memset(user_data->oldData, 0xff, 1024 * 1024);
+    memset(user_data->data, 0xff, 128 * 1024);
+    memset(user_data->oldData, 0xff, 128 * 1024);
 
     return user_data;
 }
@@ -74,8 +74,8 @@ static void getAddressLine(char* adressText, uint64_t address, int adressSize) {
 static void drawData(HexMemoryData* data, PDUI* uiFuncs, int lineCount, int charsPerLine) {
     uint64_t address = (uint64_t)strtol(data->startAddress, 0, 16);
     int adressSize = data->addressSize;
-    uint8_t* memoryData = data->data + address;
-    uint8_t* oldMemoryData = data->oldData + address;
+    uint8_t* memoryData = data->data;
+    uint8_t* oldMemoryData = data->oldData;
 
     if (charsPerLine > 1024)
         charsPerLine = 1024;
@@ -141,15 +141,15 @@ void drawUI(HexMemoryData* data, PDUI* uiFuncs) {
     uiFuncs->push_item_width(100);
     uiFuncs->input_text("Start Address", data->startAddress, sizeof(data->startAddress), PDUIInputTextFlags_CharsHexadecimal, 0, 0);
     uiFuncs->same_line(0, -1);
-    uiFuncs->input_text("End Address", data->endAddress, sizeof(data->endAddress), 0, 0, 0);
+    uiFuncs->input_text("Size", data->sizeText, sizeof(data->sizeText), 0, 0, 0);
     uiFuncs->pop_item_width();
 
-    PDVec2 size = { 0.0f, 0.0f };
+    PDVec2 child_size = { 0.0f, 0.0f };
 
     long startAddress = strtol(data->startAddress, 0, 16);
-    long endAddress = strtol(data->endAddress, 0, 16);
+    long size = strtol(data->sizeText, 0, 16);
 
-    if ((endAddress >= startAddress) && !data->data)
+    if (!data->data)
         return;
 
     if (data->sa != (uint64_t)startAddress) {
@@ -157,15 +157,15 @@ void drawUI(HexMemoryData* data, PDUI* uiFuncs) {
         data->sa = (uint64_t)startAddress;
     }
 
-    if (data->ea != (uint64_t)endAddress) {
+    if (data->size != (uint64_t)size) {
         data->requestData = true;
-        data->ea = (uint64_t)endAddress;
+        data->size = (uint64_t)size;
     }
 
     //PDVec2 textStart = uiFuncs->get_cursor_pos();
     PDVec2 windowSize = uiFuncs->get_window_size();
 
-    uiFuncs->begin_child("child", size, false, 0);
+    uiFuncs->begin_child("child", child_size, false, 0);
 
     //PDRect rect = uiFuncs->getCurrentClipRect();
     //PDVec2 pos = uiFuncs->get_window_pos();
@@ -178,7 +178,7 @@ void drawUI(HexMemoryData* data, PDUI* uiFuncs) {
 
     float drawableChars = (float)(int)(windowSize.x / (fontWidth + 23));
 
-    int drawableLineCount = (int)((endAddress - startAddress) / (int)drawableChars);
+    int drawableLineCount = (int)((size) / (int)drawableChars);
 
     //printf("%d %d %d %d\n", drawableLineCount, (int)endAddress, (int)startAddress, (int)drawableChars);
 
@@ -192,30 +192,20 @@ void drawUI(HexMemoryData* data, PDUI* uiFuncs) {
 static void updateMemory(HexMemoryData* user_data, PDReader* reader) {
     void* data;
     uint64_t address = 0;
-    uint64_t size = 0;
+    uint64_t size = user_data->size;
 
     PDRead_find_u64(reader, &address, "address", 0);
 
     if (PDRead_find_data(reader, &data, &size, "data", 0) == PDReadStatus_NotFound)
         return;
 
-    //printf("%s(%d) update memory\n", __FILE__, __LINE__);
-
-    // TODO: Currently we just copy the memory into a predefined memory range. This needs to really be fixed
-    // We need some overlapping memory ranges and such here to handle this correctly
-
-    if (address + size >= 1024 * 1024) {
-        printf("%s(%d) address (0x%16llx %x) + size larger than 1 mb which isn't supported right now\n", __FILE__, __LINE__, address, (int)size);
-        return;
-    }
-
-    // TODO: VirtualMemory manager that can requestmemory on a per 4k page or something similar instead of this
+    user_data->sa = address;
 
     // save the old data that is used for showing the changes
-    memcpy(user_data->oldData + address, user_data->data + address, (size_t)size);
+    memcpy(user_data->oldData, user_data->data, (size_t)size);
 
     // And update with the new data
-    memcpy(user_data->data + address, data, (size_t)size);
+    memcpy(user_data->data, data, (size_t)size);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -265,7 +255,7 @@ static int update(void* user_data, PDUI* uiFuncs, PDReader* inEvents, PDWriter* 
         //printf("requesting memory range %04x - %04x\n", (uint16_t)data->sa, (uint16_t)data->ea);
         PDWrite_event_begin(writer, PDEventType_GetMemory);
         PDWrite_u64(writer, "address_start", data->sa);
-        PDWrite_u64(writer, "size", data->ea - data->sa);
+        PDWrite_u64(writer, "size", data->size);
         PDWrite_event_end(writer);
     }
 
@@ -278,7 +268,7 @@ static int saveState(void* user_data, struct PDSaveState* saveState) {
     HexMemoryData* data = (HexMemoryData*)user_data;
 
     PDIO_write_string(saveState, data->startAddress);
-    PDIO_write_string(saveState, data->endAddress);
+    PDIO_write_string(saveState, data->sizeText);
 
     return 1;
 }
@@ -289,7 +279,7 @@ static int loadState(void* user_data, struct PDLoadState* loadState) {
     HexMemoryData* data = (HexMemoryData*)user_data;
 
     PDIO_read_string(loadState, data->startAddress, sizeof(data->startAddress));
-    PDIO_read_string(loadState, data->endAddress, sizeof(data->endAddress));
+    PDIO_read_string(loadState, data->sizeText, sizeof(data->sizeText));
 
     return 1;
 }
