f98426aac0395183a4e45cc5a1109809326487b5
Daniel Collin <daniel@collin.com>
2016-06-04 18:04:53 +0200

    Serialize Dock + tests

--- src/prodbg/viewdock/src/lib.rs
+++ src/prodbg/viewdock/src/lib.rs
@@ -635,7 +635,7 @@ impl Workspace {
 mod test {
     extern crate serde_json;
 
-    use {Split, Workspace, Rect, DockHandle, SplitHandle, Direction};
+    use {Split, Workspace, Dock, Rect, DockHandle, SplitHandle, Direction};
 
     fn check_range(inv: f32, value: f32, delta: f32) -> bool {
         (inv - value).abs() < delta
@@ -788,5 +788,56 @@ mod test {
         let handle_out: SplitHandle = serde_json::from_str(&serialized).unwrap();
 
         assert_eq!(handle_in, handle_out);
+    }
+
+    #[test]
+    fn test_dock_serialize_0() {
+        let dock_in = Dock {
+            handle: DockHandle(1),
+            plugin_name: "disassembly".to_owned(),
+            plugin_data: None,
+            rect: Rect::new(1.0, 2.0, 3.0, 4.0)
+        };
+
+        let serialized = serde_json::to_string(&dock_in).unwrap();
+        let dock_out: Dock = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(dock_in.handle, dock_out.handle);
+        assert_eq!(dock_in.plugin_name, dock_out.plugin_name);
+        assert_eq!(dock_in.plugin_data, dock_out.plugin_data);
+        // expect that rect is not serialized and set to zero
+        assert_eq!(dock_out.rect.x as i32, 0);
+        assert_eq!(dock_out.rect.y as i32, 0);
+        assert_eq!(dock_out.rect.width as i32, 0);
+        assert_eq!(dock_out.rect.height as i32, 0);
+    }
+
+    #[test]
+    fn test_dock_serialize_1() {
+        let dock_in = Dock {
+            handle: DockHandle(1),
+            plugin_name: "registers".to_owned(),
+            plugin_data: Some(vec!["some_data".to_owned(), "more_data".to_owned()]),
+            rect: Rect::new(4.0, 5.0, 2.0, 8.0)
+        };
+
+        let serialized = serde_json::to_string(&dock_in).unwrap();
+        let dock_out: Dock = serde_json::from_str(&serialized).unwrap();
+
+        assert_eq!(dock_in.handle, dock_out.handle);
+        assert_eq!(dock_in.plugin_name, dock_out.plugin_name);
+        assert_eq!(dock_in.plugin_data, dock_out.plugin_data);
+
+        // expect that rect is not serialized and set to zero
+        assert_eq!(dock_out.rect.x as i32, 1);
+        assert_eq!(dock_out.rect.y as i32, 0);
+        assert_eq!(dock_out.rect.width as i32, 0);
+        assert_eq!(dock_out.rect.height as i32, 0);
+
+        let plugin_data = dock_out.plugin_data.as_ref().unwrap();
+
+        assert_eq!(plugin_data.len(), 2);
+        assert_eq!(plugin_data[0], "some_data");
+        assert_eq!(plugin_data[1], "more_data");
     }
 }
