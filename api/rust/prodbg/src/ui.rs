368e7aa0e70d4d1077da9d8c7cb4e0b0e864515e
Daniel Collin <daniel@collin.com>
2016-06-16 19:21:50 +0200

    Added keyboard support for Rust view plugins

--- api/rust/prodbg/src/ui.rs
+++ api/rust/prodbg/src/ui.rs
@@ -7,6 +7,135 @@ use std::os::raw::{c_void};
 
 use CFixedString;
 
+/// Taken from minifb. Not sure if we can share this somehow?
+/// Key is used by the get key functions to check if some keys on the keyboard has been pressed
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Key {
+    Key0 = 0,
+    Key1 = 1,
+    Key2 = 2,
+    Key3 = 3,
+    Key4 = 4,
+    Key5 = 5,
+    Key6 = 6,
+    Key7 = 7,
+    Key8 = 8,
+    Key9 = 9,
+
+    A = 10,
+    B = 11,
+    C = 12,
+    D = 13,
+    E = 14,
+    F = 15,
+    G = 16,
+    H = 17,
+    I = 18,
+    J = 19,
+    K = 20,
+    L = 21,
+    M = 22,
+    N = 23,
+    O = 24,
+    P = 25,
+    Q = 26,
+    R = 27,
+    S = 28,
+    T = 29,
+    U = 30,
+    V = 31,
+    W = 32,
+    X = 33,
+    Y = 34,
+    Z = 35,
+
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+    F13,
+    F14,
+    F15,
+
+    Down,
+    Left,
+    Right,
+    Up,
+    Apostrophe,
+    Backquote,
+
+    Backslash,
+    Comma,
+    Equal,
+    LeftBracket,
+    Minus,
+    Period,
+    RightBracket,
+    Semicolon,
+
+    Slash,
+    Backspace,
+    Delete,
+    End,
+    Enter,
+
+    Escape,
+
+    Home,
+    Insert,
+    Menu,
+
+    PageDown,
+    PageUp,
+
+    Pause,
+    Space,
+    Tab,
+    NumLock,
+    CapsLock,
+    ScrollLock,
+    LeftShift,
+    RightShift,
+    LeftCtrl,
+    RightCtrl,
+
+    NumPad0,
+    NumPad1,
+    NumPad2,
+    NumPad3,
+    NumPad4,
+    NumPad5,
+    NumPad6,
+    NumPad7,
+    NumPad8,
+    NumPad9,
+    NumPadDot,
+    NumPadSlash,
+    NumPadAsterisk,
+    NumPadMinus,
+    NumPadPlus,
+    NumPadEnter,
+
+    LeftAlt,
+    RightAlt,
+
+    LeftSuper,
+    RightSuper,
+
+    /// Used when an Unknown key has been pressed
+    Unknown,
+
+    Count = 107,
+}
+
 #[derive(Clone)]
 pub struct Ui {
     pub api: *mut CPdUI,
@@ -319,6 +448,27 @@ impl Ui {
 	                        height as f32, None))
         }
     }
+
+
+    ///
+    /// Keyboard support
+    ///
+
+	pub fn is_key_down(&self, key: Key) -> bool {
+        unsafe { int_to_bool!(((*self.api).is_key_down)(key as i32)) }
+    }
+
+	pub fn is_key_pressed(&self, key: Key, repeat: bool) -> bool {
+        unsafe { int_to_bool!(((*self.api).is_key_pressed)(key as i32, true_is_1!(repeat))) }
+    }
+
+	pub fn is_key_released(&self, key: Key) -> bool {
+        unsafe { int_to_bool!(((*self.api).is_key_released)(key as i32)) }
+    }
+
+    ///
+    /// Rendering primitives
+    ///
 
     pub fn fill_convex_poly(&self, vertices: &[Vec2], col: Color, anti_aliased: bool) {
 	    unsafe {
