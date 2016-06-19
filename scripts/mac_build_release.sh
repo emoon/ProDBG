068c07c6bde2c53966011eb9747bfd3000102ee6
Daniel Collin <daniel@collin.com>
2016-06-19 12:21:29 +0200

    Removed old building code

--- scripts/mac_build_release.sh
+++ scripts/mac_build_release.sh
@@ -1,6 +1,3 @@
-#!/bin/bash 
+#!/bin/bash
 bin/macosx/tundra/tundra2 macosx-clang-release
-cargo build --release
-cargo build --manifest-path=src/plugins/bitmap_memory/Cargo.toml --release
-cargo build --manifest-path=src/prodbg/tests/rust_api_test/Cargo.toml --release
 
