7cd2f6b441c78db03f2820a29be62e12f7b45c9d
Daniel Collin <daniel@collin.com>
2016-06-24 19:37:39 +0200

    Removed more unused files

--- scripts/unix_coverity_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rfv cov-int
-rm -rfv prodbg_cov.xz
-tundra2 -c linux-gcc-debug
-cov-build --dir cov-int tundra2 -v linux-gcc-debug
-tar cavf prodbg_cov.xz cov-int
-curl --form token=gBMZ9SVfbnqwMUGd7JkIvA --form email=daniel@collin.com --form file=@prodbg_cov.xz --form version="0.01" --form description="Linux GCC" https://scan.coverity.com/builds?project=emoon%2FProDBG
