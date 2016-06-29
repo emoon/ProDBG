51f445ded92681c46eaaf77f199d2abb7ed7faa0
Vyacheslav <SlNPacifist@gmail.com>
2016-06-29 22:03:47 +0800

    Window.views now removes deleted plugin's handles properly (#211)
    Added undo functionality (currently bound to 'Z' key)

--- src/prodbg/viewdock/src/area/mod.rs
+++ src/prodbg/viewdock/src/area/mod.rs
@@ -146,11 +146,11 @@ mod test {
             0.7,
             SplitHandle(513),
             Rect::new(17.0, 15.0, 100.0, 159.0),
-            Area::Container(Container::new(Dock::new(DockHandle(14)), Rect::default())),
-            Area::Container(Container::new(Dock::new(DockHandle(15)), Rect::default()))
+            Area::Container(Container::new(Dock::new(DockHandle(14), "test"), Rect::default())),
+            Area::Container(Container::new(Dock::new(DockHandle(15), "test2"), Rect::default()))
         ));
         let container = Area::Container(Container::new(
-            Dock::new(DockHandle(17)), Rect::new(1.0, 2.0, 3.0, 4.0)
+            Dock::new(DockHandle(17), "test"), Rect::new(1.0, 2.0, 3.0, 4.0)
         ));
         let split_serialized = serde_json::to_string(&split).unwrap();
         let container_serialized = serde_json::to_string(&container).unwrap();
