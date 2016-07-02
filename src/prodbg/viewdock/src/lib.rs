8010cf16ce53bdfd8ed5b1f19a23e9459779b34c
slnpacifist <slnpacifist@gmail.com>
2016-07-02 19:43:56 +0800

    +TODO

--- src/prodbg/viewdock/src/lib.rs
+++ src/prodbg/viewdock/src/lib.rs
@@ -116,6 +116,7 @@ impl Workspace {
         })
     }
 
+    // TODO: rename into `update_rect`
     pub fn update(&mut self, new_rect: Rect) {
         self.rect = new_rect;
         if let Some(ref mut a) = self.root_area {
@@ -123,6 +124,8 @@ impl Workspace {
         }
     }
 
+    // TODO: rename into `change_ratio`
+    // TODO: use absolute coordinates here
     pub fn drag_sizer(&mut self, handle: SplitHandle, index: usize, delta: (f32, f32)) {
         if let Some(ref mut root) = self.root_area {
             if let Some(s) = root.get_split_by_handle(handle) {
