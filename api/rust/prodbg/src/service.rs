0f8f60c8c4f5a12afc99ba2a344836dfaa3c8d95
Daniel Collin <daniel@collin.com>
2016-07-03 22:47:23 +0200

    Added get_id_register service
    This allows plugins to register custom ids for custom events being sent between backend and views. This is used in UAE backend for example for UAE DMA timing data.

--- api/rust/prodbg/src/service.rs
+++ api/rust/prodbg/src/service.rs
@@ -7,6 +7,9 @@ use CCapstone1;
 use Dialogs;
 use CDialogFuncs1;
 
+use IdFuncs;
+use CIdFuncs1;
+
 pub struct Service {
     pub service_func: extern "C" fn(data: *const c_uchar) -> *mut c_void,
 }
@@ -14,8 +17,7 @@ pub struct Service {
 impl Service {
     pub fn get_capstone(&self) -> Capstone {
         unsafe {
-            let api: &mut CCapstone1 = transmute(((*self).service_func)(b"Capstone Service 1\0"
-                                                                            .as_ptr()));
+            let api: &mut CCapstone1 = transmute(((*self).service_func)(b"Capstone Service 1\0".as_ptr()));
             Capstone {
                 api: api,
                 handle: ::std::ptr::null(),
@@ -25,9 +27,15 @@ impl Service {
 
     pub fn get_dialogs(&self) -> Dialogs {
         unsafe {
-            let api: &mut CDialogFuncs1 = transmute(((*self).service_func)(b"Dialogs 1\0"
-                                                                               .as_ptr()));
+            let api: &mut CDialogFuncs1 = transmute(((*self).service_func)(b"Dialogs 1\0".as_ptr()));
             Dialogs { api: api }
+        }
+    }
+
+    pub fn get_id_register(&self) -> IdFuncs {
+        unsafe {
+            let api: &mut CIdFuncs1 = transmute(((*self).service_func)(b"IdFuncs 1\0".as_ptr()));
+            IdFuncs { api: api }
         }
     }
 }
--- src/prodbg/core/src/services.rs
+++ src/prodbg/core/src/services.rs
@@ -1,6 +1,7 @@
 use std::os::raw::{c_void, c_char, c_uchar};
 use std::ffi::CStr;
 use std::ptr;
+use prodbg_api::id_register;
 
 pub extern "C" fn get_services(type_name: *const c_uchar) -> *mut c_void {
     unsafe {
@@ -8,6 +9,7 @@ pub extern "C" fn get_services(type_name: *const c_uchar) -> *mut c_void {
 
         match name {
             "Capstone Service 1" => get_capstone_service_1(),
+            "IdFuncs 1" => id_register::get_id_register_funcs(),
             _ =>  ptr::null_mut(),
         }
     }
