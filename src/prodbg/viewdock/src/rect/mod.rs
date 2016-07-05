36dbd80853c8fafff80d0bbdfa00218f6fbff7ef
slnpacifist <slnpacifist@gmail.com>
2016-07-05 14:38:37 +0800

    Added docs and tests for Rect

--- src/prodbg/viewdock/src/rect/mod.rs
+++ src/prodbg/viewdock/src/rect/mod.rs
@@ -6,7 +6,9 @@ pub enum Direction {
     Horizontal,
 }
 
+/// Defines direction for most of rectangle methods
 impl Direction {
+    /// Returns opposite direction for given
     pub fn opposite(&self) -> Direction {
         match *self {
             Direction::Vertical => Direction::Horizontal,
@@ -15,6 +17,7 @@ impl Direction {
     }
 }
 
+/// Defines a way to shrink rectangle. See Rectangle::shrinked for more.
 #[derive(Debug, Clone)]
 pub enum ShrinkSide {
     Lower,
@@ -31,6 +34,7 @@ pub struct Rect {
     pub height: f32,
 }
 
+/// Simple axis-aligned rectangle defined by its left top corner, width and height
 impl Rect {
     pub fn new(x: f32, y: f32, width: f32, height: f32) -> Rect {
         Rect {
@@ -41,6 +45,7 @@ impl Rect {
         }
     }
 
+    /// Returns true if `pos` is inside of rectangle
     pub fn point_is_inside(&self, pos: (f32, f32)) -> bool {
         let (x, y) = pos;
         return
@@ -50,19 +55,7 @@ impl Rect {
             self.y + self.height >= y;
     }
 
-    pub fn split(&self, direction: Direction, size: f32) -> [Rect; 2] {
-        match direction {
-            Direction::Horizontal => [
-                Rect::new(self.x, self.y, self.width, size),
-                Rect::new(self.x, self.y + self.height - size, self.width, size),
-            ],
-            Direction::Vertical => [
-                Rect::new(self.x, self.y, size, self.height),
-                Rect::new(self.x + self.width - size, self.y, size, self.height),
-            ]
-        }
-    }
-
+    /// Returns size of rectangle in given `direction`
     pub fn dimension(&self, direction: Direction) -> f32 {
         match direction {
             Direction::Horizontal => self.width,
@@ -70,6 +63,7 @@ impl Rect {
         }
     }
 
+    /// Returns new rectangle shifted by `dist` in given `direction`
     pub fn shifted(&self, direction: Direction, dist: f32) -> Rect {
         match direction {
             Direction::Horizontal => Rect::new(self.x + dist, self.y, self.width, self.height),
@@ -84,6 +78,19 @@ impl Rect {
         }
     }
 
+    /// Returns new rectangle shrinked by `size` in `direction`. If `side` is `ShrinkSide::Lower`,
+    /// rectangle will shrinked from top or left. If `side` is `ShrinkSide::Higher`, rectangle
+    /// will be shrinked from right or bottom. If `side` is `ShrinkSide::Both`, new rectangle will
+    /// be shrinked from both sides of given `direction`.
+    /// For example, if `origin` is `Rect {x: 0.0, y: 0.0, width: 10.0, height: 20.0}`, then
+    /// `origin.shrinked(Direction::Horizontal, 5.0, ShrinkSide::Lower)` will be
+    /// `Rect {x: 5.0, y: 0.0, width: 5.0, height: 20.0}`.
+    ///
+    /// `origin.shrinked(Direction::Vertical, 5.0, ShrinkSide::Higher)` will be
+    /// `Rect {x: 0.0, y: 0.0, width: 10.0, height: 15.0}`.
+    ///
+    /// `origin.shrinked(Direction::Vertical, 2.0, ShrinkSide::Both)` will be
+    /// `Rect {x: 0.0, y: 2.0, width: 10.0, height: 16.0}`.
     pub fn shrinked(&self, direction: Direction, size: f32, side: ShrinkSide) -> Rect {
         let mut res = self.clone();
         {
@@ -108,6 +115,12 @@ impl Rect {
         return res;
     }
 
+    /// Returns rectangles for each value in `ratios`. Each `ratio` is relative to width or height.
+    /// `direction` shows direction of splits.
+    /// For example, if `origin` is `Rect {x: 0.0, y: 0.0, width: 10.0, height: 20.0}`, then
+    /// `origin.area_around_splits(Direction::Horizontal, &[0.4, 0.8], 2.0)` will be vector of
+    /// `Rect {x: 0.0, y: 7.0, width: 10.0, height: 2.0}` and
+    /// `Rect {x: 0.0, y: 15.0, width: 10.0, height: 2.0}`
     pub fn area_around_splits(&self, direction: Direction, ratios: &[f32], width: f32) -> Vec<Rect> {
         match direction {
             Direction::Horizontal => {
@@ -125,6 +138,8 @@ impl Rect {
         }
     }
 
+    /// Splits rectangle into several smaller rectangles. `ratios` contains values relative to
+    /// corresponding dimension (i.e. 0.5 is half of width or height).
     pub fn split_by_direction(&self, direction: Direction, ratios: &[f32]) -> Vec<Rect> {
         match direction {
             Direction::Horizontal => Rect::split_horizontally(self, ratios),
@@ -132,23 +147,23 @@ impl Rect {
         }
     }
 
-    pub fn split_horizontally(rect: &Rect, ratios: &[f32]) -> Vec<Rect> {
+    fn split_horizontally(&self, ratios: &[f32]) -> Vec<Rect> {
         let mut prev_height = 0.0;
         return ratios.iter().map(|ratio| {
-            let next_height = rect.height * ratio;
+            let next_height = self.height * ratio;
             let rect_height = next_height - prev_height;
-            let res = Rect::new(rect.x, rect.y + prev_height, rect.width, rect_height);
+            let res = Rect::new(self.x, self.y + prev_height, self.width, rect_height);
             prev_height = next_height;
             return res;
         }).collect();
     }
 
-    pub fn split_vertically(rect: &Rect, ratios: &[f32]) -> Vec<Rect> {
+    fn split_vertically(&self, ratios: &[f32]) -> Vec<Rect> {
         let mut prev_width = 0.0;
         return ratios.iter().map(|ratio| {
-            let next_width = rect.width * ratio;
+            let next_width = self.width * ratio;
             let rect_width = next_width - prev_width;
-            let res = Rect::new(rect.x + prev_width, rect.y, rect_width, rect.height);
+            let res = Rect::new(self.x + prev_width, self.y, rect_width, self.height);
             prev_width = next_width;
             return res;
         }).collect();
@@ -158,56 +173,82 @@ impl Rect {
 #[cfg(test)]
 mod test {
     extern crate serde_json;
-    use {Rect, Direction};
+    use super::{Rect, Direction, ShrinkSide};
 
-    fn check_range(inv: f32, value: f32, delta: f32) -> bool {
-        (inv - value).abs() < delta
+    fn floats_are_equal(inv: f32, value: f32) -> bool {
+        (inv - value).abs() < 1.19209290e-07f32
     }
 
-//    #[test]
-//    fn test_calc_rect_horz_half() {
-//        let rects = Rect::split_horizontally(&Rect::new(0.0, 0.0, 1024.0, 1024.0), 0.5);
-//
-//        assert_eq!(check_range(rects.0.x, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.y, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.width, 1024.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.height, 512.0, 0.001), true);
-//
-//        assert_eq!(check_range(rects.1.x, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.y, 512.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.width, 1024.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.height, 512.0, 0.001), true);
-//    }
-//
-//    #[test]
-//    fn test_calc_rect_horz_25_per() {
-//        let rects = Rect::split_horizontally(&Rect::new(0.0, 0.0, 1024.0, 1024.0), 0.25);
-//
-//        assert_eq!(check_range(rects.0.x, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.y, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.width, 1024.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.height, 256.0, 0.001), true);
-//
-//        assert_eq!(check_range(rects.1.x, 0.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.y, 256.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.width, 1024.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.height, 768.0, 0.001), true);
-//    }
-//
-//    #[test]
-//    fn test_calc_rect_horz_25_per_2() {
-//        let rects = Rect::split_horizontally(&Rect::new(16.0, 32.0, 512.0, 1024.0), 0.25);
-//
-//        assert_eq!(check_range(rects.0.x, 16.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.y, 32.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.width, 512.0, 0.001), true);
-//        assert_eq!(check_range(rects.0.height, 256.0, 0.001), true);
-//
-//        assert_eq!(check_range(rects.1.x, 16.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.y, 288.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.width, 512.0, 0.001), true);
-//        assert_eq!(check_range(rects.1.height, 768.0, 0.001), true);
-//    }
+    fn rects_are_equal(first: Rect, second: Rect) -> bool {
+        floats_are_equal(first.x, second.x)
+        && floats_are_equal(first.y, second.y)
+        && floats_are_equal(first.width, second.width)
+        && floats_are_equal(first.height, second.height)
+    }
+
+    #[test]
+    fn test_point_is_inside() {
+        let rect = Rect::new(0.0, 0.0, 10.0, 10.0);
+        assert!(rect.point_is_inside((5.0, 5.0)));
+        assert!(rect.point_is_inside((1.0, 5.0)));
+        assert!(!rect.point_is_inside((-1.0, 5.0)));
+        assert!(!rect.point_is_inside((11.0, 5.0)));
+        assert!(!rect.point_is_inside((1.0, -5.0)));
+        assert!(!rect.point_is_inside((1.0, 55.0)));
+    }
+
+    #[test]
+    fn test_shifted() {
+        let rect = Rect::new(0.0, 0.0, 10.0, 10.0);
+        assert!(rects_are_equal(Rect::new(2.0, 0.0, 10.0, 10.0), rect.shifted(Direction::Horizontal, 2.0)));
+        assert!(rects_are_equal(Rect::new(0.0, -5.0, 10.0, 10.0), rect.shifted(Direction::Vertical, -5.0)));
+    }
+
+    #[test]
+    fn test_shrinked() {
+        let rect = Rect::new(0.0, 0.0, 10.0, 20.0);
+        assert!(rects_are_equal(
+            Rect::new(5.0, 0.0, 5.0, 20.0),
+            rect.shrinked(Direction::Horizontal, 5.0, ShrinkSide::Lower)
+        ));
+        assert!(rects_are_equal(
+            Rect::new(0.0, 0.0, 10.0, 15.0),
+            rect.shrinked(Direction::Vertical, 5.0, ShrinkSide::Higher)
+        ));
+        assert!(rects_are_equal(
+            Rect::new(0.0, 2.0, 10.0, 16.0),
+            rect.shrinked(Direction::Vertical, 2.0, ShrinkSide::Both)
+        ));
+    }
+
+    #[test]
+    fn test_area_around_splits() {
+        let rect = Rect::new(0.0, 0.0, 10.0, 20.0);
+        let res = rect.area_around_splits(Direction::Horizontal, &[0.4, 0.8], 2.0);
+        assert_eq!(2, res.len());
+        assert!(rects_are_equal(Rect::new(0.0, 7.0, 10.0, 2.0), res[0]));
+        assert!(rects_are_equal(Rect::new(0.0, 15.0, 10.0, 2.0), res[1]));
+        let res2 = rect.area_around_splits(Direction::Vertical, &[0.25, 0.5, 1.0], 2.0);
+        assert_eq!(3, res2.len());
+        assert!(rects_are_equal(Rect::new(1.5, 0.0, 2.0, 20.0), res2[0]));
+        assert!(rects_are_equal(Rect::new(4.0, 0.0, 2.0, 20.0), res2[1]));
+        assert!(rects_are_equal(Rect::new(9.0, 0.0, 2.0, 20.0), res2[2]));
+    }
+
+    #[test]
+    fn split_by_direction() {
+        let rect = Rect::new(0.0, 0.0, 1024.0, 1024.0);
+        let res = rect.split_by_direction(Direction::Horizontal, &[0.25, 0.5, 1.0]);
+        assert_eq!(3, res.len());
+        assert!(rects_are_equal(Rect::new(0.0, 0.0, 1024.0, 256.0), res[0]));
+        assert!(rects_are_equal(Rect::new(0.0, 256.0, 1024.0, 256.0), res[1]));
+        assert!(rects_are_equal(Rect::new(0.0, 512.0, 1024.0, 512.0), res[2]));
+        let res2 = rect.split_by_direction(Direction::Vertical, &[0.25, 0.5, 1.0]);
+        assert_eq!(3, res.len());
+        assert!(rects_are_equal(Rect::new(0.0, 0.0, 256.0, 1024.0), res2[0]));
+        assert!(rects_are_equal(Rect::new(256.0, 0.0, 256.0, 1024.0), res2[1]));
+        assert!(rects_are_equal(Rect::new(512.0, 0.0, 512.0, 1024.0), res2[2]));
+    }
 
     #[test]
     fn test_rect_serialization() {
@@ -235,40 +276,4 @@ mod test {
         assert_eq!(dir_in_0, dir_out_0);
         assert_eq!(dir_in_1, dir_out_1);
     }
-//
-//    TODO: update following tests for area around split calculation
-//    #[test]
-//    fn test_gen_horizontal_size() {
-//        let border_size = 4.0;
-//        let rect_in = Rect::new(10.0, 20.0, 30.0, 40.0);
-//        let rect = rect_in.area_around_split(border_size);
-//
-//        assert_eq!(check_range(rect.x, rect_in.x, 0.001), true);
-//        assert_eq!(check_range(rect.y, 60.0, 0.001), true);
-//        assert_eq!(check_range(rect.width, rect_in.width, 0.001), true);
-//        assert_eq!(check_range(rect.height, border_size, 0.001), true);
-//    }
-//
-//    #[test]
-//    fn test_gen_vertical_size() {
-//        let border_size = 4.0;
-//        let rect_in = Rect::new(10.0, 20.0, 30.0, 40.0);
-//        let rect = Split::get_sizer_from_rect_vertical(rect_in, border_size);
-//
-//        assert_eq!(check_range(rect.x, 40.0, 0.001), true);
-//        assert_eq!(check_range(rect.y, rect_in.y, 0.001), true);
-//        assert_eq!(check_range(rect.width, border_size, 0.001), true);
-//        assert_eq!(check_range(rect.height, rect_in.height, 0.001), true);
-//    }
-
-//    #[test]
-//    fn test_inside_horizontal() {
-//        let border_size = 4.0;
-//        let rect = Rect::new(10.0, 20.0, 30.0, 40.0);
-//        let rect_horz = Split::get_sizer_from_rect_horizontal(rect, border_size);
-//
-//        assert_eq!(Rect::is_inside((9.0, 61.0), rect_horz), false);
-//        assert_eq!(Rect::is_inside((11.0, 61.0), rect_horz), true);
-//    }
-
 }
