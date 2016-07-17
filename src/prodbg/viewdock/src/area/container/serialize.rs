cefb5a1a6a256bd64444c121845dba94abf72e9b
slnpacifist <slnpacifist@gmail.com>
2016-07-17 22:57:07 +0800

    Added macros for structs deserialization in Viewdock.

--- src/prodbg/viewdock/src/area/container/serialize.rs
+++ src/prodbg/viewdock/src/area/container/serialize.rs
@@ -24,73 +24,9 @@ impl<'a> serde::ser::MapVisitor for ContainerMapVisitor<'a> {
 
 // Deserialization
 
-impl serde::Deserialize for Container {
-    fn deserialize<D>(deserializer: &mut D) -> Result<Container, D::Error> where D: serde::de::Deserializer {
-        static FIELDS: &'static [&'static str] = &["docks"];
-        deserializer.deserialize_struct("Container", FIELDS, ContainerVisitor)
-    }
-}
-
-struct ContainerVisitor;
-
-impl serde::de::Visitor for ContainerVisitor {
-    type Value = Container;
-
-    fn visit_map<V>(&mut self, mut visitor: V) -> Result<Container, V::Error> where V: serde::de::MapVisitor {
-        let mut docks = None;
-        let mut active_dock = None;
-
-        loop {
-            match try!(visitor.visit_key()) {
-                Some(ContainerField::Docks) => { docks = Some(try!(visitor.visit_value())); }
-                Some(ContainerField::ActiveDock) => { active_dock = Some(try!(visitor.visit_value())); }
-                None => { break; }
-            }
-        }
-
-        let docks = match docks {
-            Some(docks) => docks,
-            None => try!(visitor.missing_field("docks")),
-        };
-
-        let active_dock = match active_dock {
-            Some(active_dock) => active_dock,
-            None => try!(visitor.missing_field("active_dock")),
-        };
-
-        try!(visitor.end());
-
-        Ok(Container {
-            docks: docks,
-            tab_borders: vec!(0.0),
-            active_dock: active_dock,
-            rect: Rect::default(), // We use default here as this is always recalculated
-        })
-    }
-}
-
-enum ContainerField {
-    Docks,
-    ActiveDock
-}
-
-impl serde::Deserialize for ContainerField  {
-    fn deserialize<D>(deserializer: &mut D) -> Result<ContainerField, D::Error> where D: serde::de::Deserializer {
-        struct ContainerFieldVisitor;
-
-        impl serde::de::Visitor for ContainerFieldVisitor {
-            type Value = ContainerField;
-
-            fn visit_str<E>(&mut self, value: &str) -> Result<ContainerField, E>
-                where E: serde::de::Error {
-                    match value {
-                        "docks" => Ok(ContainerField::Docks),
-                        "active_dock" => Ok(ContainerField::ActiveDock),
-                        _ => Err(serde::de::Error::custom("expected docks")),
-                    }
-                }
-        }
-
-        deserializer.deserialize(ContainerFieldVisitor)
-    }
-}
+gen_struct_deserializer!(Container;
+    docks => "docks", Docks,
+    active_dock => "active_dock", ActiveDock;
+    tab_borders => vec!(0.0)
+    rect => Rect::default()
+);
--- src/prodbg/viewdock/src/dock/serialize.rs
+++ src/prodbg/viewdock/src/dock/serialize.rs
@@ -25,82 +25,8 @@ impl<'a> serde::ser::MapVisitor for DockMapVisitor<'a> {
 }
 
 // Deserialization
-
-impl serde::Deserialize for Dock {
-    fn deserialize<D>(deserializer: &mut D) -> Result<Dock, D::Error> where D: serde::de::Deserializer {
-        static FIELDS: &'static [&'static str] = &["handle", "plugin_name", "plugin_data"];
-        deserializer.deserialize_struct("Dock", FIELDS, DockVisitor)
-    }
-}
-
-struct DockVisitor;
-
-impl serde::de::Visitor for DockVisitor {
-    type Value = Dock;
-
-    fn visit_map<V>(&mut self, mut visitor: V) -> Result<Dock, V::Error> where V: serde::de::MapVisitor {
-        let mut handle = None;
-        let mut plugin_name = None;
-        let mut plugin_data = None;
-
-        loop {
-            match try!(visitor.visit_key()) {
-                Some(DockField::Handle) => { handle = Some(try!(visitor.visit_value())); }
-                Some(DockField::PluginName) => { plugin_name = Some(try!(visitor.visit_value())); }
-                Some(DockField::PluginData) => { plugin_data = Some(try!(visitor.visit_value())); }
-                None => { break; }
-            }
-        }
-
-        let handle = match handle {
-            Some(handle) => handle,
-            None => try!(visitor.missing_field("handle")),
-        };
-
-        let plugin_name = match plugin_name {
-            Some(plugin_name) => plugin_name,
-            None => try!(visitor.missing_field("plugin_name")),
-        };
-
-        let plugin_data = match plugin_data {
-            Some(plugin_data) => plugin_data,
-            None => try!(visitor.missing_field("plugin_data")),
-        };
-
-        try!(visitor.end());
-
-        Ok(Dock {
-            handle: handle,
-            plugin_name: plugin_name,
-            plugin_data: plugin_data,
-        })
-    }
-}
-
-enum DockField {
-    Handle,
-    PluginName,
-    PluginData,
-}
-
-impl serde::Deserialize for DockField  {
-    fn deserialize<D>(deserializer: &mut D) -> Result<DockField, D::Error> where D: serde::de::Deserializer {
-        struct DockFieldVisitor;
-
-        impl serde::de::Visitor for DockFieldVisitor {
-            type Value = DockField;
-
-            fn visit_str<E>(&mut self, value: &str) -> Result<DockField, E>
-                where E: serde::de::Error {
-                    match value {
-                        "handle" => Ok(DockField::Handle),
-                        "plugin_name" => Ok(DockField::PluginName),
-                        "plugin_data" => Ok(DockField::PluginData),
-                        _ => Err(serde::de::Error::custom("expected handle,plugin_name or pluin_data")),
-                    }
-                }
-        }
-
-        deserializer.deserialize(DockFieldVisitor)
-    }
-}
+gen_struct_deserializer!(Dock;
+    handle => "handle", Handle,
+    plugin_name => "plugin_name", PluginName,
+    plugin_data => "plugin_data", PluginData;
+);
