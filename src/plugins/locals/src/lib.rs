fa1626b929d39612af379d26da0badf9fb9e6c48
Daniel Collin <daniel@collin.com>
2016-09-03 07:56:09 -0700

    Fixed warnings

--- src/plugins/locals/src/lib.rs
+++ src/plugins/locals/src/lib.rs
@@ -7,7 +7,7 @@ use prodbg_api::events::*;
 
 
 struct LocalsView {
-    dummy: u8
+    _dummy: u8
 }
 
 impl LocalsView {
@@ -20,9 +20,9 @@ impl LocalsView {
          ui.text("Type"); ui.next_column();
 
          for entry in reader.find_array("threads").unwrap() {
-             let name = entry.find_string("name").unwrap();
-             let value = entry.find_string("value").unwrap();
-             let type_cell = entry.find_string("type").unwrap();
+             let _name = entry.find_string("name").unwrap();
+             let _value = entry.find_string("value").unwrap();
+             let _type_cell = entry.find_string("type").unwrap();
          }
      }
 }
@@ -30,11 +30,11 @@ impl LocalsView {
 impl View for LocalsView {
     fn new(_: &Ui, _: &Service) -> Self {
         LocalsView {
-            dummy: 0u8
+            _dummy: 0u8
         }
     }
 
-    fn update(&mut self, ui: &mut Ui, reader: &mut Reader, writer: &mut Writer) {
+    fn update(&mut self, ui: &mut Ui, reader: &mut Reader, _writer: &mut Writer) {
 
         for event in reader.get_events() {
             match event {
@@ -49,9 +49,8 @@ impl View for LocalsView {
         // Request callstack data
         // TODO: Dont' request locals all the time
 
-        writer.event_begin(EVENT_GET_LOCALS as u16);
-        writer.write_u8("dummy_remove", 0); // TODO Remove me
-        writer.event_end();
+        //writer.event_begin(EVENT_GET_LOCALS as u16);
+        //writer.event_end();
     }
 }
 
