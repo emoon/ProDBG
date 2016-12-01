e72756e1200f08ecc3f040f92d2bd18e6641354a
Daniel Collin <daniel@collin.com>
2016-12-01 15:30:15 +0100

    Now using a MainWindow with some docking

--- src/prodbg/MainWindow.cpp
+++ src/prodbg/MainWindow.cpp
@@ -1,9 +1,38 @@
 #include "MainWindow.h"
 #include "MemoryView/MemoryViewWidget.h"
 #include "CodeView/CodeView.h"
+#include <QDockWidget>
+
+namespace prodbg {
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 MainWindow::MainWindow() : 
-	m_memoryView(new MemoryViewWidget),
 	m_codeView(new CodeView),
+	m_memoryView(new MemoryViewWidget)
 {
+    char* dummy_data = (char*)malloc(1024 * 1024);
+
+    for (int i = 0; i < 1024 * 1024; ++i)
+        dummy_data[i] = (char)rand();
+
+    m_memoryView->setData(QByteArray(dummy_data, 1024 * 1024));
+
+	setCentralWidget(m_codeView);
+
+	setWindowTitle(tr("ProDBG"));
+
+	QDockWidget *dock = new QDockWidget(tr("MemoryView"), this);
+    dock->setAllowedAreas(Qt::AllDockWidgetAreas);
+
+    dock->setWidget(m_memoryView);
+    addDockWidget(Qt::RightDockWidgetArea, dock);
+
+    m_codeView->readSourceFile("src/prodbg/main.cpp");
+
+    resize(1024, 768);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
--- src/prodbg/main.cpp
+++ src/prodbg/main.cpp
@@ -1,28 +1,21 @@
+#include "MainWindow.h"
 #include <QApplication>
 #include <QTextEdit>
-#include "MemoryView/MemoryViewWidget.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, const char** argv) {
+int main(int argc, const char** argv)
+{
     QApplication app(argc, (char**)argv);
 
-    char* dummy_data = (char*)malloc(1024 * 1024);
+    prodbg::MainWindow main_window;
 
-    for (int i = 0; i < 1024 * 1024; ++i)
-        dummy_data[i] = (char)rand();
+    main_window.show();
 
-    prodbg::MemoryViewWidget hex_edit;
-
-    hex_edit.setData(QByteArray(dummy_data, 1024 * 1024));
-
-    hex_edit.show();
-
-    //QTextEdit text_edit;
-    //text_edit.show();
+    // QTextEdit text_edit;
+    // text_edit.show();
 
     return app.exec();
 }
-
