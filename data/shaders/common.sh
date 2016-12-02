730ee8c539b3faf0df183c959fe9b9df98470d2c
Daniel Collin <daniel@collin.com>
2016-12-02 11:40:43 +0100

    Removed old stuff

--- data/shaders/common.sh
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2011-2014 Branimir Karadzic. All rights reserved.
- * License: http://www.opensource.org/licenses/BSD-2-Clause
- */
-
-#include "bgfx_shader.sh"
-#include "shaderlib.sh"
--- data/shaders/ui_pos_color/varying.def.sc
+++
@@ -1,5 +0,0 @@
-vec4 v_color0 : COLOR0  = vec4(0.0, 0.0, 0.0, 0.0);
-
-vec2 a_position  : POSITION;
-vec4 a_color0    : COLOR0;
-
