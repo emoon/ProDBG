45c7ae45f9b17896419393d1e88db88f8b3ecde8
Daniel Collin <daniel@collin.com>
2016-12-03 13:15:19 +0100

    Clang format

--- api/include/pd_backend.h
+++ api/include/pd_backend.h
@@ -2,8 +2,8 @@
 #define _PRODBGAPI_BACKEND_H_
 
 #include "pd_common.h"
-#include "pd_readwrite.h"
 #include "pd_menu.h"
+#include "pd_readwrite.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -86,7 +86,7 @@ typedef enum PDEventType {
     PDEventType_ExecuteConsole,
     PDEventType_GetConsole,
 
-	PDEventType_MenuEvent,
+    PDEventType_MenuEvent,
 
     // TODO: Somewhat temporary, need to figure this out
 
@@ -110,7 +110,8 @@ typedef enum PDEventType {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-typedef struct PDBackendPlugin {
+typedef struct PDBackendPlugin
+{
     const char* name;
 
     void* (*create_instance)(ServiceFunc* service_func);
@@ -118,8 +119,8 @@ typedef struct PDBackendPlugin {
     PDDebugState (*update)(void* user_data, PDAction action, PDReader* reader, PDWriter* writer);
 
     // save/load state
-	int (*save_state)(void* user_data, struct PDSaveState* save_state);
-	int (*load_state)(void* user_data, struct PDLoadState* load_state);
+    int (*save_state)(void* user_data, struct PDSaveState* save_state);
+    int (*load_state)(void* user_data, struct PDLoadState* load_state);
 
 } PDBackendPlugin;
 
@@ -139,4 +140,3 @@ typedef struct PDBackendPlugin {
 #endif
 
 #endif
-
