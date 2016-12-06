eea2e75d4ed8ccf664917d473924232f8e7a2721
Daniel Collin <daniel@collin.com>
2016-12-06 10:42:13 +0100

    Commends and more data for requests

--- src/prodbg/Backend/IBackendRequests.h
+++ src/prodbg/Backend/IBackendRequests.h
@@ -15,9 +15,47 @@ class IBackendRequests : public QObject
     Q_OBJECT;
 
 public:
+
+    // 
+    // Description of a hardware register commonly found in most CPUs
+    //
+    struct Register
+    {
+        // Name of the register
+        QString name;
+        // If the register is read only (hw registers such as Program Counter)
+        bool read_only;
+        // Data for the register (stored in big endian order)
+        QByteArray data;
+    };
+
+    //
+    // Description of assembly instruction that is provided when requesting
+    // disassemble of code fro the target
+    //
+    struct AssemblyInstruction
+    {
+        // Text of the instruction (like move.w d0,d1 or mov rax,rbx)
+        QString text;
+        // Address on where the instruction was stored
+        uint64_t address;
+        // List of hw registers that this instruction reads
+        // This may be empty if the backend doesn't support to fill this info in
+        QVector<QString> read_registers;
+        // List of hw registers that this instrution reads
+        // This may be empty if the backend doesn't support to fill this info in
+        QVector<QString> write_registers;
+    };
+
+    //
+    // Describes the memory as sent back from beginReadMemory. These flags
+    // indicates what kind of memory it is (read/write/unmapped/etc)
+    //
     enum MemoryAddressFlags
     {
+        // Set if memory is read-able
         Readable = 1 << 8,
+        // Set if memory is write-able
         Writable = 1 << 9
     };
 
