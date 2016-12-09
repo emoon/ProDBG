bc33215fd8965b83e47fe500d1df4a8b2b8eab91
Daniel Collin <daniel@collin.com>
2016-12-09 07:37:44 +0100

    Dark color scheme testing

--- src/prodbg/main.cpp
+++ src/prodbg/main.cpp
@@ -8,6 +8,7 @@
 #include <QTextStream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <QStyleFactory>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -18,6 +19,30 @@ int main(int argc, const char** argv)
     QCoreApplication::setOrganizationName(QStringLiteral("TBL"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("prodbg.com"));
     QCoreApplication::setApplicationName(QStringLiteral("ProDBG"));
+
+    /*
+    app.setStyle(QStyleFactory::create(QStringLiteral("Fusion")));
+ 
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        
+    app.setPalette(darkPalette);
+    
+    app.setStyleSheet(QStringLiteral("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }"));
+    */
 
     // load hard-coded list of backends for now
 
