247e61877455631dc4313971ce5472a1b1acf3e2
Daniel Collin <daniel@collin.com>
2017-05-07 16:58:38 +0200

    Add more comman structure for plugin in Rust

--- src/plugins/memory_view_2/src/lib.rs
+++ src/plugins/memory_view_2/src/lib.rs
@@ -1,14 +1,49 @@
 extern crate prodbg_ui;
 
+use std::mem::transmute;
+use std::os::raw::{c_void};
+
 //use prodbg_ui::*;
+//
+
+struct MyPlugin {
+    _dummy: i32,
+}
+
+impl MyPlugin {
+    fn new() -> Self {
+        MyPlugin {
+            _dummy: 0,
+        }
+    }
+
+    /*
+    fn pressed_button(&mut self) {
+
+    }
+    */
+
+    fn create_ui(&mut self, ui: &prodbg_ui::ui_gen::Ui) {
+        let button = ui.push_button_create();
+        button.set_title("Rust: Test");
+        //button.on_pressed(self, MyPlugin::pressed_button);
+    }
+}
+
 
 #[no_mangle]
 pub fn init_plugin(ui_ptr: *const prodbg_ui::ffi_gen::PU) {
+    // This stuff here is really hidden from the plugin
+    // but we do this temporary when testing out the apis
     let ui = prodbg_ui::ui_gen::Ui::new(ui_ptr);
 
-    //
+    // This is some tricker to (later on) send back a instance of the plugin to C and then on the
+    // Rust side again cast it back to a Rust instance
 
-    ui.push_button_create();
+    let instance: *mut c_void  = unsafe { transmute(Box::new(MyPlugin::new())) };
+    let plugin: &mut MyPlugin = unsafe { &mut *(instance as *mut MyPlugin) };
+
+    plugin.create_ui(&ui);
 
     println!("Rust: memory_view_2");
 }
