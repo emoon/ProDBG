b20d43ee8dfa0ebbc50705b6e0bfb0d5e5d8a805
Daniel Collin <daniel@collin.com>
2020-07-23 09:47:32 +0200

    Missed file

--- api/include/pd_backend_messages.h
+++ api/include/pd_backend_messages.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "PDMessages_generated.h"
+
+template<typename T>
+inline void PDMessage_end_msg(PDWriter* writer, T& msg, flatbuffers::FlatBufferBuilder& builder) {
+    builder.Finish(CreateMessageDirect(builder,
+        MessageTypeTraits<typename T::Table>::enum_value, msg.Finish().Union()));
+
+    printf("reply enum %d\n", MessageTypeTraits<T>::enum_value);
+
+    // TODO: Streamline this
+    PDWrite_event_begin(writer, PDEventType_Dummy);
+    PDWrite_data(writer, "data", builder.GetBufferPointer(), builder.GetSize());
+    PDWrite_event_end(writer);
+}
+
